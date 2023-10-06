1fc5f514cf88e3a01ded08a2ea363cfbf3a15d7c
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-10-06 22:16:02 +0530

    new problem added

---
+++ Apples_and_oranges.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include<numeric>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+        int n,m;
+        cin>>n>>m;
+cout<<_gcd(n,m)<<endl;
+	}
+	return 0;
+}
