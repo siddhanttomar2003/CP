8230b857150a417d9109d455567dfe881f85b2de
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-10-15 09:44:37 +0530

    new prob

---
+++ Equal_Card_Game.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    int eq=52%k;
+	    cout<<eq<<endl;
+	}
+	return 0;
+}
