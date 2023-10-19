f538232e9f920467bd7df491807d29cff94c085d
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-10-19 23:00:00 +0530

    prob

---
+++ Fitness.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void problemSolution()
+{
+  int x;
+  cin >> x;
+  cout << x * 10 << "\n";
+}
+
+void testCases()
+{
+  int tt;
+  cin >> tt;
+  while(tt--)
+  {
+    problemSolution();
+  }
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  testCases();
+  // problemSolution();
+  return 0;
+}
