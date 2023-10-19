bb7efff0e2928f0d1848277c48b947eaaecb0b9c
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-10-19 23:00:47 +0530

    prob

---
+++ Chef_Plays_Ludo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    if(x==6)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
