0add563f492ff79275fbc8781a770d5f048c7c3f
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-10-20 23:24:45 +0530

    new prob

---
+++ Coins_And_Triangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;cin>>n;
+	    int i=1;int h=0;
+	    while(n/i>0){ h++;n-=i;i++;}
+	    cout<<h<<endl;
+	}
+	return 0;
+}
