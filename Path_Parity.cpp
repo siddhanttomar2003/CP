fffaea970ccf4cde8ca77870c9d77dd479f4828b
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-10-22 15:03:40 +0530

    new prob

---
+++ Path_Parity.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    if(n%2!=0&&k==0)cout<<"NO"<<endl;
+	    else cout<<"YES"<<endl;
+          	                                 
+	}
+	return 0;
+}
