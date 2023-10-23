2a4e6faf8b7c48801e4608c1f1c248b3c90d6a60
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-10-23 14:29:28 +0530

    new prob

---
+++ Plus_2_or_Minus_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n==0)
+        cout<<"1"<<endl;
+        else
+        cout<<3*n<<endl;
+    }
+	// your code goes here
+	return 0;
+}
