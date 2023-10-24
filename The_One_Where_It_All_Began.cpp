3c54cc18356e0b8c9591caf14a8cb333589691d1
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-10-24 16:44:26 +0530

    new prob

---
+++ The_One_Where_It_All_Began.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	 
+          	int n;
+          	cin>>n;
+          	if(n>=21){
+          	    cout<<"YES"<<endl;
+          	}
+          	else cout<<"NO"<<endl;
+	}
+	return 0;
+}
