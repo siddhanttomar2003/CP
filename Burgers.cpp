51b80bacfa658363f5ffd4fd2fdbf12435efade5
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-11-01 22:52:58 +0530

    new prob

---
+++ Burgers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<min(a,b)<<endl;
+	    
+	}
+	return 0;
+}
