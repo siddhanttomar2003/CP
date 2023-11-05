4564c20bb7429432519da321d252d4b918983ad0
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-11-05 15:44:19 +0530

    new prob

--- B_The_Corridor_or_There_and_Back_Again.cpp
+++ B_The_Corridor_or_There_and_Back_Again.cpp
@@ -8,29 +8,18 @@ int main() {
 	int t;
 	cin>>t;
 	while(t--){
-	 int n;
-     cin>>n;
-     int d[n];
-     int s[n];
-     for(int i=0;i<n;i++){
-        cin>>d[i];
-        cin>>s[i];
-     }
-     
-     int ans=0;
-     if(n==1){
-        ans+=s[0]/2+d[0];
-     }
-     else{
-     for(int i=1;i<n;i++){
-     if(s[i-1]/2<d[i]-d[i-1]){
-        ans=s[i-1]/2+d[i-1];
-     }
-     }
-     }
-     cout<<ans<<endl;
-
-          	                                 
+	 
+          int n;
+          cin>>n;
+          long long d[n],s[n];
+          for(int i=0;i<n;i++){
+            cin>>d[i]>>s[i];
+          }	  
+          long long k=INT64_MAX;
+            for(int i=0;i<n;i++){
+              k=min(d[i]+(s[i]-1)/2,k);
+            }  
+            cout<<k<<endl;                             
 	}
 	return 0;
 }
