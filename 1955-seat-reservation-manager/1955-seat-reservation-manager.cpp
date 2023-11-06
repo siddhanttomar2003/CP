5c6e1dde2a41535e38db53d0c1fb16af83d44b75
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2023-11-06 19:12:30 +0530

    Time: 481 ms (11.48%), Space: 190.9 MB (12.34%) - LeetHub

---
+++ 1955-seat-reservation-manager/1955-seat-reservation-manager.cpp
@@ -0,0 +1,17 @@
+class SeatManager {
+public:
+    set<int> st; //Create a set to keep track of unreserved seats
+    SeatManager(int n) {
+        for(int i=1;i<=n;i++) st.insert(i); // Insert all the values to set
+    }
+    
+    int reserve() {
+        int val = *st.begin(); //find out the first element of set, because it stores the data in ascending order
+        st.erase(val); //Remove this from unreserved list
+        return val;
+    }
+    
+    void unreserve(int seatNumber) {
+        st.insert(seatNumber); //insert the given seat number to unreserved list
+    }
+};
