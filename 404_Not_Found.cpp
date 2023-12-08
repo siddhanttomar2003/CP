de08f163f8c37043fd81714dd0d5e3c43c70ede7
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2023-12-08 11:02:53 +0530

    new prob

---
+++ 404_Not_Found.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int x;
+	cin>>x;
+	if(x==404)cout<<"NOT FOUND"<<endl;
+	else cout<<"FOUND"<<endl;
+	return 0;
+}
