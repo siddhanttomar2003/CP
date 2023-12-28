d1d76e1cca76a0a61bf35799995b6a58cc709751
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2023-12-29 00:22:30 +0530

    Time: 10 ms (50.18%), Space: 12.2 MB (62.13%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int mid=0;
-        int mid1=0;
+       
         int moves=0;
         if(nums.size()%2!=0){
-           mid=nums[nums.size()/2];
+          nth_element(nums.begin(),nums.begin()+nums.size()/2,nums.end());
            
            for(int i=0;i<nums.size();i++){
-               moves+=abs(nums[i]-mid);
+               moves+=abs(nums[i]-nums[nums.size()/2]);
            }
         }
           else{
-              mid=nums[nums.size()/2];
-              mid1=nums[nums.size()/2-1];
+              
               int moves1=0;
               int moves2=0;
+              nth_element(nums.begin(),nums.begin()+nums.size()/2,nums.end());
+              nth_element(nums.begin(),nums.begin()+nums.size()/2-1,nums.end());
               for(int i=0;i<nums.size();i++){
-                  moves1+=abs(nums[i]-mid);
-                  moves2+=abs(nums[i]-mid1);
+                  moves1+=abs(nums[i]-nums[nums.size()/2]);
+                  moves2+=abs(nums[i]-nums[nums.size()/2-1]);
               }
               moves=min(moves1,moves2);
           }
