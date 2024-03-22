a3bedcece25967327ffcb86263ab76772a8bbb4d
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-03-22 15:54:54 +0530

    Time: 176 ms (48.94%), Space: 119 MB (61.44%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,15 +10,31 @@
  */
 class Solution {
 public:
+     ListNode * reverse(ListNode *head){
+         if(head==NULL||head->next==NULL){
+             return head;
+         }
+         ListNode *smallans=reverse(head->next);
+         ListNode *temp=head->next;
+         temp->next=head;
+         head->next=NULL;
+         return smallans;
+     }
     bool isPalindrome(ListNode* head) {
-        string s="";
-        ListNode *temp=head;
-        while(temp!=NULL){
-         s+=temp->val;
-         temp=temp->next;
+        ListNode *start=head;
+        if(head==NULL||head->next==NULL)return true;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast->next!=NULL&&fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        string t=s;
-        reverse(s.begin(),s.end());
-        return t==s;
+        ListNode *temp=reverse(slow->next);
+        while(temp!=NULL){
+            if(start->val!=temp->val)return false;
+            start=start->next;
+            temp=temp->next;
+        }
+        return true;
     }
 };
