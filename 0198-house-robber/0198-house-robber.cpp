f310126b21f4070aded49fdbf09e3b202767c305
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-03-30 15:56:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int recursive(vector<int> &nums,int i,vector<int> &dp){
-        if(i==0)return nums[0];
-        if(i<0)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int pick=nums[i]+recursive(nums,i-2,dp);
-        int not_pick=recursive(nums,i-1,dp);
-        dp[i]=max(pick,not_pick);
-        return max(pick,not_pick);
-
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n+1,-1);
-        int i=nums.size()-1;
-        return  recursive(nums,i,dp);
+        prev=nums[0];
+        prev2=INT_MIN;
+        for(int i=1;i<n;i++){
+            int pick=INT_MIN;
+            int not_pick=INT_MIN;
+            if(i>1)pick=nums[i]+prev2;
+           
+            not_pick=prev;
+            int curr=max(pick,not_pick);
+            
+            prev=curr;
+           
+        }
+
+        return dp[n-1];
     }
 };
