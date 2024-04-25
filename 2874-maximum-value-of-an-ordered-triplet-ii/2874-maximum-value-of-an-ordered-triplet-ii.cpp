109f3cd92f2ad8f636bba8437ffeb1bbf9d42e5f
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-04-25 17:02:13 +0530

    Time: 115 ms (40.23%), Space: 96.4 MB (49.04%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -9,16 +9,23 @@ public:
             next_greater[n-i-1]=maxi;
             maxi=max(maxi,nums[n-i-1]);
         }
+        int mmaxi=0;
+        vector<int>prev_greater(n,0);
+        for(int i=0;i<n;i++){
+            prev_greater[i]=mmaxi;
+            mmaxi=max(mmaxi,nums[i]);
+        }
         long long ans=0;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]>nums[j]){
-                    long long a,b,c;
-                    a=nums[i];b=nums[j];c=next_greater[j];
-                    ans=max(ans,(a-b)*c);
-                }
-            }
+           if(prev_greater[i]>nums[i]){
+            long long a=prev_greater[i];
+            long long b=nums[i];
+            long long  c=next_greater[i];
+            ans=max(ans,(a-b)*c);
+           }
         }
+       
+        
         return ans;
     }
 };
