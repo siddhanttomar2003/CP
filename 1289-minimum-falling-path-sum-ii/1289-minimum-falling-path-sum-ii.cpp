42fb669984beb2820a3879e292e74631f10e11b9
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-04-30 18:41:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-     int minpath(vector<vector<int>>& grid, vector<vector<int>>&dp,int i,int j){
-        if(i==grid.size()-1){
-            int mini=INT_MAX;
-            for(int c=0;c<grid.size();c++){
-                if(c!=j)mini=min(mini,grid[i][c]);
-            }
-            return  dp[i][j]=mini;
-        }
-        if(dp[i][j]!=INT_MAX)return dp[i][j];
-        for(int c=0;c<grid.size();c++){
-            if(c!=j){
-               dp[i][j]= min(dp[i][j],grid[i][c]+minpath(grid,dp,i+1,c));
-            }
-        }
-        return dp[i][j];
-     }
+    //  int minpath(vector<vector<int>>& grid, vector<vector<int>>&dp,int i,int j){
+    //     if(i==grid.size()-1){
+    //         int mini=INT_MAX;
+    //         for(int c=0;c<grid.size();c++){
+    //             if(c!=j)mini=min(mini,grid[i][c]);
+    //         }
+    //         return  dp[i][j]=mini;
+    //     }
+    //     if(dp[i][j]!=INT_MAX)return dp[i][j];
+    //     for(int c=0;c<grid.size();c++){
+    //         if(c!=j){
+    //            dp[i][j]= min(dp[i][j],grid[i][c]+minpath(grid,dp,i+1,c));
+    //         }
+    //     }
+    //     return dp[i][j];
+    //  }
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         if(n==1)return grid[0][0];
-        vector<vector<int>>dp(n,vector<int>(n+1,INT_MAX));
-        minpath(grid,dp,0,n);
-        return *min_element(dp[0].begin(),dp[0].end());
+         vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
+         for(int i=0;i<grid.size();i++){
+            dp[0][i]=grid[0][i];
+         }
+         for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int mini=INT_MAX;
+                for(int k=0;k<n;k++){
+                    if(j==k)continue;
+                    dp[i][j]=min(dp[i][j],grid[i][k]+dp[i-1][j]);
+                }
+            }
+         }
+         return *min_element(dp[n-1].begin(),dp[n-1].end());
     }
 };
