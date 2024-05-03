30608b0e741f12c18a0292139e7480e3e2f64cbd
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-05-03 15:35:11 +0530

    Time: 305 ms (5%), Space: 20.5 MB (5.83%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -4,18 +4,17 @@ public:
         if(i>=nums.size()-1){
           return 0;
         }
-        if((dp[i]!=-1))return dp[i];
-        int turn =1e8;
+        if((dp[i]!=1e8))return dp[i];
         for(int j=1;j<=nums[i];j++){
-            turn = min(turn ,1+recur(nums,i+j,dp));
+            dp[i]= min(dp[i] ,1+recur(nums,i+j,dp));
         }
-       return  dp[i]=turn;
+       return  dp[i];
 
      }
     int jump(vector<int>& nums) {
         int i=0;
         int n=nums.size();
-        vector<int>dp(n,-1);
+        vector<int>dp(n,1e8);
        return recur(nums,i,dp);
         
     }
