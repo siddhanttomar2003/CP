afd1015011ab7818a948422c143054ad3ef97b34
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-05-03 15:37:28 +0530

    Time: 89 ms (16.55%), Space: 19.7 MB (14.84%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-     int recur(vector<int> &nums, int i,vector<int> &dp){
-        if(i>=nums.size()-1){
-          return 0;
-        }
-        if((dp[i]!=1e8))return dp[i];
-        for(int j=1;j<=nums[i];j++){
-            dp[i]= min(dp[i] ,1+recur(nums,i+j,dp));
-        }
-       return  dp[i];
+    //  int recur(vector<int> &nums, int i,vector<int> &dp){
+    //     if(i>=nums.size()-1){
+    //       return 0;
+    //     }
+    //     if((dp[i]!=1e8))return dp[i];
+    //     for(int j=1;j<=nums[i];j++){
+    //         dp[i]= min(dp[i] ,1+recur(nums,i+j,dp));
+    //     }
+    //    return  dp[i];
 
-     }
+    //  }
     int jump(vector<int>& nums) {
         int i=0;
         int n=nums.size();
         vector<int>dp(n,1e8);
-       return recur(nums,i,dp);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            for(int j=1;j<=nums[i];j++){
+                if(i+j<n)
+                dp[i]=min(dp[i],1+dp[i+j]);
+            }
+        }
+        return dp[0];
         
     }
 };
