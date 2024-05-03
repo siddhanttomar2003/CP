349787613eb13f4f1687cf3bf57d85edce16bf86
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-05-03 21:38:56 +0530

    Time: 85 ms (48.36%), Space: 63.6 MB (75.69%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int Smaller_than_mid(vector<int>&nums,int &mid){
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<=mid)count++;
-        }
-        return count;
-    }
     int findDuplicate(vector<int>& nums) {
-       // binary search on answers
-       // we know that the answer will always be in the range of 1->n
-       int s=1;
-       int e=nums.size()-1;
-       while(s<e){
-        int mid=s+(e-s)/2;
-        int count=Smaller_than_mid(nums,mid);
-        if(count>mid){// means answer lies on the left side including mid
-        e=mid;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                if(nums[nums[i]-1]==nums[i])continue;
+                else {
+                    swap(nums[nums[i]-1],nums[i]);
+                    i--;
+                }
+            }
         }
-        else s=mid+1;
-       }
-       return e;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1)return nums[i];
+        }
+        return 0;
+
     }
-   
 };
