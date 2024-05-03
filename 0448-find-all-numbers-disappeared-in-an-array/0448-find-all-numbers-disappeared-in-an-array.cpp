3aa8fed96db9768a5f253f61fbdc447e0c7d9be2
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-05-03 21:28:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -3,15 +3,27 @@ public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         // using hashmap
         int n=nums.size();
-        unordered_set<int>s;
-        for(int i=0;i<n;i++){
-                  s.insert(nums[i]);
+    //     unordered_set<int>s;
+    //     for(int i=0;i<n;i++){
+    //               s.insert(nums[i]);
+    //     }
+    //     int num=1;
+    //     vector<int>ans;
+    //    for(int i=1;i<=n;i++){
+    //         if(s.find(i)==s.end())ans.push_back(i);
+    //    }
+    //    return ans;
+    // now using cyclic sort
+     for(int i=0;i<n;i++){
+        if(nums[i]!=i-1){
+            if(nums[nums[i]-1]==nums[i])continue;
+            else {swap(nums[nums[i]-1],nums[i]);i--;}
         }
-        int num=1;
-        vector<int>ans;
-       for(int i=1;i<=n;i++){
-            if(s.find(i)==s.end())ans.push_back(i);
-       }
-       return ans;
+     }
+     vector<int>Ans;
+     for(int i=0;i<n;i++){
+        if(nums[i]!=i+1)Ans.push_back(i+1);
+     }
+     return Ans;
     }
 };
