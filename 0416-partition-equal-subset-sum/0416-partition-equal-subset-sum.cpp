0c0ee0bbc43a4f83b05b31474624ae2d2b59160b
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-05-05 23:25:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
-    bool recur(vector<int> &nums,int take,int nottake,int i,vector<vector<vector<int>>>&dp){
-        if(i==nums.size()){
-            if(take==nottake)return true;
-            else return false;
-        }
-        if(dp[i][take][nottake]!=-1)return dp[i][take][nottake];
-        take+=nums[i];
-      bool ans1=  recur(nums,take,nottake,i+1,dp);
-        take-=nums[i];
-        nottake+=nums[i];
-      bool ans2=   recur(nums,take,nottake,i+1,dp);
-        nottake-=nums[i];
-        return dp[i][take][nottake]= ans1||ans2;
-
-    }
+    // bool recur(vector<int> &nums,vector<vector<int>> &dp, int sum, int i, int take){
+    //     if(i==nums.size()){
+    //         if(take==sum/2){
+    //             return true;
+    //         }
+    //         return false;
+    //     }
+    //     if(take==sum/2){
+    //      return   dp[i][take]=true;
+    //     }
+    //     if(dp[i][take]!=-1)return dp[i][take];
+    //      take+=nums[i];
+    //    bool ans1=  recur(nums,dp,sum,i+1,take);
+    //      take-=nums[i];
+    //      bool ans2=recur(nums,dp,sum,i+1,take);
+    //     return   dp[i][take]=ans1 || ans2;
+    // }
     bool canPartition(vector<int>& nums) {
-        int take=0;
-        int nottake=0;
         int sum=0;
         sum=accumulate(nums.begin(),nums.end(),sum);
         if(sum&1)return false;
-        
-        vector<vector<vector<int>>>dp(nums.size(),vector<vector<int>>(sum,vector<int>(sum,-1)));
-       return recur(nums,take,nottake,0,dp);
-    
+        // if sum is even there must be some method to divide the array into 
+        // two parts having equal sum
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int> (sum+1,0));
+         for(int i=0;i<n;i++){
+         dp[i][0]=1;
+         }
+         dp[0][nums[0]]=1;
+         for(int i=1;i<n;i++){
+            for(int j=1;j<=sum;j++){
+                bool take=false;
+                if(nums[i]<=j)take=dp[i-1][j-nums[i]];
+                bool nottake=false;
+               nottake=dp[i-1][j];
+               dp[i][j]=take|nottake;
+            }
+
+         }
+         return dp[n-1][sum];
+         
+
     }
 };
