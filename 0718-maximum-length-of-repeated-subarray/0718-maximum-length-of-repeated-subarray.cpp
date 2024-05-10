b341869b4cade3dfbaa4722e9c916ef266ce1b75
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2024-05-10 15:12:22 +0530

    Time: 2594 ms (5%), Space: 88.2 MB (6.9%) - LeetHub

---
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int find_max(vector<int> &nums1,vector<int> & nums2,int index1,int index2,vector<vector<int>> &dp){
+    // base case
+     if(index1<0 || index2<0)return 0;
+      if(dp[index1][index2]!=-1)return dp[index1][index2];
+    int take=0;
+    if(nums1[index1]==nums2[index2]){
+        int i=index1;int j=index2;
+        while(i>=0 && j>=0){
+            if(nums1[i]==nums2[j]){
+                take++;
+                i--;j--;
+            }
+            else break;
+        }
+    }
+    int nottake=max(find_max(nums1,nums2,index1-1,index2,dp),find_max(nums1,nums2,index1,index2-1,dp));
+      return dp[index1][index2]=max(take,nottake);
+    
+    }
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        // using dp
+        // we can use two indexes to find out till that what is the max substring we can get
+        int index1=nums1.size();
+        int index2=nums2.size();
+        vector<vector<int>>dp(index1,vector<int>(index2,-1));
+          return  find_max(nums1,nums2,index1-1,index2-1,dp);
+    }
+};
