ba1ffb6ec41f48c82cd3a2439f9c3b7662e36072
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-05-23 21:14:01 +0530

    Time: 0 ms (100%), Space: 9.2 MB (34.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,6 +11,19 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        return find_max(dp,nums,n-1);
+        // now tabulating it
+        // first write the base case
+        // go in reverse direction as in memo
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int take=0;
+            int nottake=0;
+            if(i>1)take=nums[i]+dp[i-2];
+            else take=nums[i];
+            nottake=dp[i-1];
+            dp[i]=max(take,nottake);
+        }
+        if(n==2)return max(nums[0],nums[1]);
+        return dp[n-1];
     }
 };
