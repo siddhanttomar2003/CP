e75cbb953c53c9567498c61540dab091d2659a54
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-05-30 00:54:01 +0530

    Time: 4 ms (93.74%), Space: 22.3 MB (83.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int>left_max(n,0);
-        vector<int>right_max(n,0);
-          int maxil=height[0];
-          int maxir=height[n-1];
-        for(int i=0;i<height.size();i++){
-            left_max[i]=maxil;
-            maxil=max(maxil,height[i]);
-            right_max[n-i-1]=maxir;
-            maxir=max(maxir,height[n-i-1]);
-        }
+        int l=0;
+        int r=n-1;
+        int maxLeft=0;
+        int maxRight=0;
         int ans=0;
-        for(int i=0;i<n;i++){
-            int mini=min(left_max[i],right_max[i]);
-            if(mini-height[i]>0)ans+=abs(height[i]-mini);
-            // else the water is flowing throught one or both the ends
+        while(l<=r){
+           if(height[l]<=height[r]){
+            if(height[l]>=maxLeft){
+                maxLeft=height[l];
+            }
+            else {
+                ans+=(maxLeft-height[l]);
+            }
+            l++;
+           }
+           else {
+            if(height[r]>=maxRight){
+                maxRight=height[r];
+            }
+            else {
+                ans+=(maxRight-height[r]);
+            }
+              r--;
+           }
+         
         }
-
-        for(int i=0;i<height.size();i++){
-            cout<<left_max[i]<<" ";
-        }
-        cout<<endl;
-        for(int i=0;i<height.size();i++){
-            cout<<right_max[i]<<" ";
-        }
-        cout<<endl;
         return ans;
-
     }
 };
