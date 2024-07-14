3a2b7ea61e9882c38674d606bbf7bc75402ec144
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-07-14 15:55:03 +0530

    Time: 9 ms (67.56%), Space: 22.7 MB (44.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size();
-        int l=0;
-        int r=n-1;
-        int maxLeft=0;
-        int maxRight=0;
-        int ans=0;
-        while(l<=r){
-           if(height[l]<=height[r]){
-            if(height[l]>=maxLeft){
-                maxLeft=height[l];
+        int l=0;int ans=0;
+        int r=height.size()-1;int left_max=0;int right_max=0;
+        while(l<r){
+         if(height[l]<=height[r]){// in this its only possible that whether the water will be stored by height[l] block or not , it further depends on left_max as in according to right it can store the water 
+          if(height[l]<left_max){
+            ans+=(left_max-height[l]);
+          }
+          else {
+            left_max=height[l];
+          }
+          l++;
+
+         }
+         else {
+            if(height[r]<right_max){
+                ans+=(right_max-height[r]);
             }
             else {
-                ans+=(maxLeft-height[l]);
+                right_max=height[r];
             }
-            l++;
-           }
-           else {
-            if(height[r]>=maxRight){
-                maxRight=height[r];
-            }
-            else {
-                ans+=(maxRight-height[r]);
-            }
-              r--;
-           }
-         
+            r--;
+         }
         }
         return ans;
     }
