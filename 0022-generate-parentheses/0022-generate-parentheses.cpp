b1444a26ae0e9332714f707d19368a0ff98c4a6a
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-07-20 18:57:07 +0530

    Time: 7 ms (22.81%), Space: 15.2 MB (43.21%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
-    void recur(int i,vector<string>&ans,int n,string &temp ){
-        if(i==2*n){
+     void gen(vector<string> &ans, string temp, int n , int o, int c){
+        if(o==n && c==n){
             ans.push_back(temp);
             return;
         }
-        // open
-        temp+='(';
-        recur(i+1,ans,n,temp);
-        temp.pop_back();
-        temp+=')';
-        recur(i+1,ans,n,temp);
-        temp.pop_back();
-
-    }
-    bool check(string s){
-        stack<char>st;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='(')st.push(s[i]);
-            else {
-                if(st.size()>0){
-                    st.pop();
-                }
-                else return false;
-            }
+        if(o<n){
+        gen(ans,temp+'(',n,o+1,c);
         }
-        if(st.size()>0)return false;
-        return true;
-    }
+        if(o>c)gen(ans,temp+')',n,o,c+1);
+     }
     vector<string> generateParenthesis(int n) {
-        int i=0;
         string temp="";
         vector<string>ans;
-        recur(i,ans,n,temp);
-        vector<string>f;
-        for(int i=0;i<ans.size();i++){
-          if(check(ans[i]))f.push_back(ans[i]);
-        }
-        return f;
+        gen(ans,temp,n,0,0);
+        return ans;
     }
 };
