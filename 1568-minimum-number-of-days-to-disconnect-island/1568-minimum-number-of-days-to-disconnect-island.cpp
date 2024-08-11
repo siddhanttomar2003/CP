c8617ea733c3abcaa3941c3a026cf21991041321
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-08-11 14:52:59 +0530

    Time: 744 ms (5.74%), Space: 116.6 MB (8.9%) - LeetHub

--- 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.cpp
+++ 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.cpp
@@ -1,55 +1,59 @@
 class Solution {
 public:
-   void dfs(int sr, int sc, vector<vector<int>> &grid){
-    grid[sr][sc]=0;
+   void dfs(int sr, int sc, vector<vector<int>> &grid,map<pair<int,int>,int> &mp){
+     mp[{sr,sc}]++;
     int delRow[]={-1,0,1,0};
     int delCol[]={0,1,0,-1};
     int n=grid.size();int m=grid[0].size();
     for(int i=0;i<4;i++){
         int nr=sr+delRow[i];
         int nc=sc+delCol[i];
-        if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]){
-            dfs(nr,nc,grid);
+        if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc] && mp.find({nr,nc})==mp.end()){
+            dfs(nr,nc,grid,mp);
         }
     }
+   }
+  
+   int check(vector<vector<int>> &grid){
+       int n=grid.size();int m=grid[0].size();
+       int cal=0;
+       map<pair<int,int>,int>mp;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j] && mp.find({i,j})==mp.end()){
+                cal++;
+                dfs(i,j,grid,mp);
+            }
+        }
+       }
+       return cal;
    }
     int minDays(vector<vector<int>>& grid) {
         // map<pair<int,int>,int>mp;
         int n=grid.size();int m=grid[0].size();
         int cal=0;
-        int count1=0;
+        map<pair<int,int>,int>mp;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j])count1++;
-            }
-        }
-        bool flag=false;
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<m;j++){
-                  if(grid[i][j])count++;
-            }
-            if(count==count1)flag=true;
-        }
-        for(int j=0;j<m;j++){
-            int count=0;
-            for(int i=0;i<n;i++){
-                   if(grid[i][j])count++;
-            }
-            if(count==count1)flag=true;
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]){
+                if(grid[i][j] && mp.find({i,j})==mp.end()){
                     cal++;
-                    dfs(i,j,grid);
+                    dfs(i,j,grid,mp);
                 }
             }
         }
-        if(cal>1 || cal<1)return 0;
-        else {
-              if(flag && count1>2)return 1;
-              else return 2;
+        if(cal!=1)return 0;
+        bool flag=false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]){
+                    grid[i][j]=0;
+                     int cc=check(grid);
+                     if(cc!=1)return 1;
+                     grid[i][j]=1;
+                }
+            }
         }
+
+      return 2;
     }
 };
