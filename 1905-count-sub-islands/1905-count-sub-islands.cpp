af8e5c941d3fe9d61ebb87261a3a0ed63395e3aa
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-08-28 11:05:32 +0530

    Time: 2775 ms (5.07%), Space: 217 MB (16.29%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,35 +1,65 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, bool &check,int i, int j){
-        if(grid1[i][j]==0 && grid2[i][j]==1){
-            check=false;return;
+    void dfs2(int sr, int sc, map<pair<int, int>, int>& is_visited2,
+              vector<vector<int>>& grid2, map<pair<int, int>, int>& is_visited,
+              int n, int m, bool& check) {
+        if (is_visited.find({sr, sc}) == is_visited.end()) {
+            check = false;
         }
-        int delRow[]={-1,0,1,0};
-        int delCol[]={0,-1,0,1};
-        grid2[i][j]=0;
-        for(int k=0;k<4;k++){
-            int nr=delRow[k]+i;
-            int nc=delCol[k]+j;
-            if(nr>=0 && nc>=0 && nr<grid1.size() && nc<grid1[0].size() && grid2[nr][nc]){
-                dfs(grid1,grid2, check, nr , nc);
+        is_visited2[{sr, sc}] = 1;
+        int delRow[] = {1, 0, -1, 0};
+        int delCol[] = {0, -1, 0, 1};
+        for (int i = 0; i < 4; i++) {
+            int nr = delRow[i] + sr;
+            int nc = delCol[i] + sc;
+            if (nr >= 0 && nc >= 0 && nr < n && nc < m && grid2[nr][nc] &&
+                is_visited2.find({nr, nc}) == is_visited2.end()) {
+                dfs2(nr, nc, is_visited2, grid2, is_visited, n, m, check);
             }
         }
-
+        return;
     }
-    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int n=grid1.size();
-        int m=grid1[0].size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                bool check=true;
-                if(grid2[i][j]){
-                   dfs(grid1,grid2,check,i,j);
-                   if(check)
-                    ans++;
+    void dfs(vector<vector<int>>& grid, map<pair<int, int>, int>& is_visited,
+             int sr, int sc, int n, int m) {
+        is_visited[{sr, sc}] = 1;
+        int delRow[] = {1, 0, -1, 0};
+        int delCol[] = {0, -1, 0, 1};
+        for (int i = 0; i < 4; i++) {
+            int nr = delRow[i] + sr;
+            int nc = delCol[i] + sc;
+            if (nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] &&
+                is_visited.find({nr, nc}) == is_visited.end()) {
+                dfs(grid, is_visited, nr, nc, n, m);
+            }
+        }
+    }
+    int countSubIslands(vector<vector<int>>& grid1,
+                        vector<vector<int>>& grid2) {
+        map<pair<int, int>, int> is_visited;
+        int n = grid1.size();
+        int m = grid1[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid1[i][j] == 1 &&
+                    is_visited.find({i, j}) == is_visited.end()) {
+                    dfs(grid1, is_visited, i, j, n, m);
                 }
             }
         }
-        return ans;
+        int count = 0;
+        map<pair<int, int>, int> is_visited2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid2[i][j] &&
+                    is_visited2.find({i, j}) == is_visited2.end()) {
+                    bool check = true;
+                    dfs2(i, j, is_visited2, grid2, is_visited, n, m,
+                         check) ;
+                         if (check) count++;
+                }
+            }
+        }
+
+        return count;
     }
 };
