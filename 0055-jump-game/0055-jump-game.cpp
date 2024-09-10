34c14005631d1ccbb5766691f800318480a324b2
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-09-10 16:14:14 +0530

    Time: 42 ms (67.64%), Space: 51.1 MB (81.07%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,32 +1,12 @@
 class Solution {
 public:
-//    bool recur(vector<int> & nums,int i,vector<int> &dp){
-//     if(i==nums.size()-1)return true;
-//     if(i>nums.size()-1)return false;
-//     bool ans=false;
-//     if(dp[i]!=-1)return dp[i];
-//     for(int j=1;j<=nums[i];j++){
-//       ans  =ans || recur(nums,i+j,dp);
-//     }
-//     return dp[i]=ans;
-//    }
     bool canJump(vector<int>& nums) {
-        // first using memoization
-        if(nums.size()==1)return true;
         int n=nums.size();
-        int curr=0;
-        int req=1;
-        for(int i=n-2;i>=0;i--){
-           curr=nums[i];
-           if(curr>=req){
-            req=1;
-            continue;
-           }
-           else req++;
+        int maxi=0;
+        for(int i=0;i<n;i++){
+        if(i>maxi)return false;
+         maxi=max(maxi,i+nums[i]);
         }
-        if(curr>=req)return true;
-        else return false;
-        
-
+        return true;
     }
 };
