67baf52c696579367c1999290b233b99a8e6817e
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-09-13 11:54:49 +0530

    Time: 102 ms (84.7%), Space: 77.7 MB (99.66%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int find_max(vector<int>& prices, vector<vector<int>>& dp, int i, int buy,
-                 int count) {
-        if (count == 2)
-            return 0;
-        if (i == prices.size())
-            return 0;
-        if (dp[i][buy] != -1)
-            return dp[i][buy];
-        int profit = INT_MIN;
-        if (buy) {
-            profit = max(-prices[i] + find_max(prices, dp, i + 1, 0, count),
-                         find_max(prices, dp, i + 1, 1, count));
-        } else
-            profit = max(prices[i] + find_max(prices, dp, i + 1, 1, count + 1),
-                         find_max(prices, dp, i + 1, 0, count));
-        return dp[i][buy] = profit;
-    }
     int maxProfit(vector<int>& prices) {
-        // same as part-2 just adding count variable so we cannot perform
-        // transaction more than twice
-        int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return find_max(prices, dp, 0, 1, 0);
+       int first_profit=0;
+       int first=INT_MAX;
+       int second=INT_MIN;
+       int left=INT_MIN;
+       for(int i=0;i<prices.size();i++){
+        first=min(first,prices[i]);
+        first_profit=max(first_profit,prices[i]-first);
+        left=max(left,first_profit-prices[i]);
+        second=max(second,left+prices[i]);
+       }
+       return second;
+
     }
 };
