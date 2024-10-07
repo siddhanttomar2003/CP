e3465684474e39633c7f8d00114127c8552a02f8
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-10-07 13:01:12 +0530

    Time: 2 ms (56.42%), Space: 9 MB (7.34%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -5,7 +5,6 @@ public:
         vector<string>words2;
         int n=sentence1.size();
         int m=sentence2.size();
-        if(m<n)return areSentencesSimilar(sentence2,sentence1);
         for(int i=0;i<n;i++){
             string temp="";
             while(i<n && sentence1[i]!=' '){
@@ -22,19 +21,20 @@ public:
             }
             words2.push_back(temp);
         }
+        int n1=words1.size(); int n2=words2.size();
+        if(n2<n1)return areSentencesSimilar(sentence2,sentence1);
         int i=0;
         while(i<words1.size() && words1[i]==words2[i]){
             i++;
         }
         int rem=words1.size()-i;
         if(rem==0)return true;
-        int n1=words1.size(); int n2=words2.size();int j=n2-rem; 
+      int j=n2-rem; 
         if(j<=i)return false;
         while(j<n2 && i<n1 && words1[i]==words2[j]){
             i++;j++;
         }
         return j==n2;
-
 
     }
 };
