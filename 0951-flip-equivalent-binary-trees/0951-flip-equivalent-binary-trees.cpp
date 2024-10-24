66453b47373f3731f58a7b0fd25fbcc7fa19da8c
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-10-24 09:25:54 +0530

    Time: 0 ms (100%), Space: 14.6 MB (21.11%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -17,28 +17,21 @@ public:
             return true;
         return false;
     }
-    bool cal(TreeNode* root1, TreeNode* root2) {
-         if (root1 == NULL || root2 == NULL) {
-        return root1 == root2;
-    }
-        if (isLeaf(root1) && isLeaf(root2)) {
-        return root1->val == root2->val;
-    }
-        bool cal1 = (root1->left, root2->left);
-        bool cal2 = (root1->left, root2->right);
-        bool cal3 = (root1->right, root2->left);
-        bool cal4 = (root1->right, root2->right);
-        if ((cal1 && cal4)  || (cal2 && cal4) && (root1->val == root2->val))
-            return true;
-        return false;
-    }
+
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        if (root1 == NULL && root2 != NULL || root1 != NULL && root2 == NULL)
+        if (root1 == NULL || root2 == NULL) {
+            return root1 == root2;
+        }
+        if (isLeaf(root1) && isLeaf(root2)) {
+            return root1->val == root2->val;
+        }
+        if (root1->val != root2->val) {
             return false;
-        if (!root1 && !root2)
-            return true;
-        if (root1->val != root2->val)
-            return false;
-        return cal(root1, root2);
+        }
+        bool noFlip = flipEquiv(root1->left, root2->left) &&
+                      flipEquiv(root1->right, root2->right);
+        bool flip = flipEquiv(root1->left, root2->right) &&
+                    flipEquiv(root1->right, root2->left);
+        return noFlip || flip;
     }
 };
