b17cf4c33cb7bb5fc5f13ea26819ddb822ac7a5b
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-10-29 12:05:38 +0530

    Time: 31 ms (39.13%), Space: 74.8 MB (16.51%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public: 
     int cal(vector<vector<int>>&dp,map<pair<int,int>,int>&mp, vector<vector<int>>&grid,int i , int j, int last){
-        if(i>grid.size() || j>grid[0].size())return 0;
+        if(i>=grid.size() || j>=grid[0].size() || i<0)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(mp.find({i,j})!=mp.end())return 0;
+        // if(mp.find({i,j})!=mp.end())return 0;
         int w1=0;int w2=0;int w3=0;
-        mp[{i,j}]++;
+        // mp[{i,j}]++;
         if(i+1<grid.size() && j+1<grid[0].size() && grid[i+1][j+1]>last){
-            w1=1+cal(dp,mp,grid,i+1,j+1,grid[i][j]);
+            w1=1+cal(dp,mp,grid,i+1,j+1,grid[i+1][j+1]);
         }
-        if(i-1<grid.size() && j+1<grid[0].size() && grid[i-1][j+1]>last){
-            w2=1+cal(dp,mp,grid,i-1,j+1,grid[i][j]);
+        if(i-1>=0 && j+1<grid[0].size() && grid[i-1][j+1]>last){
+            w2=1+cal(dp,mp,grid,i-1,j+1,grid[i-1][j+1]);
         }
         if(i<grid.size() && j+1<grid[0].size() && grid[i][j+1]>last){
-            w3=1+cal(dp,mp,grid,i,j+1,grid[i][j]);
+            w3=1+cal(dp,mp,grid,i,j+1,grid[i][j+1]);
         }
-        mp[{i,j}]--;
-        if(mp[{i,j}]==0)mp.erase({i,j});
+        // mp[{i,j}]--;
+        // if(mp[{i,j}]==0)mp.erase({i,j});
       return dp[i][j]=max({w1,w2,w3});
 
     }
