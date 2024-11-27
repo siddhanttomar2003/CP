0eca199a55c712e151a3c20b4119cb7b4628a324
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-11-27 17:04:11 +0530

    Time: 1472 ms (5.43%), Space: 220.4 MB (33.43%) - LeetHub

--- 3244-shortest-distance-after-road-addition-queries-ii/3244-shortest-distance-after-road-addition-queries-ii.cpp
+++ 3244-shortest-distance-after-road-addition-queries-ii/3244-shortest-distance-after-road-addition-queries-ii.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    void del(set<int>&st, int s, int e){
+        cout<<s<<" "<<e<<endl;
+        for(int i=s;i<=e;i++){
+            st.erase(i);
+        }
+    }
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
         vector<int>ans;
         set<int>st;
@@ -9,9 +15,11 @@ public:
         for(int i=0;i<queries.size();i++){
             int s=queries[i][0];
             int e=queries[i][1];
-            auto it=st.lower_bound(s);
-            auto it2=st.upper_bound(e-2);
-            st.erase(it,it2);
+            auto it1=st.upper_bound(s);
+            auto it2=st.lower_bound(e);
+            auto it3=prev(it2);
+            // cout<<*it1<<" "<<*it3<<endl;
+            del(st,*it1,*it3);
             ans.push_back(st.size()-1);
         }
         return ans;
