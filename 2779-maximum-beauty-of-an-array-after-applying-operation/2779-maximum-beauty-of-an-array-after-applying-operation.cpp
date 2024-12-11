2ef2502ea2abe7319bd2c0cbcb3b9c5b88e1aa06
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-12-11 10:19:36 +0530

    Time: 2723 ms (5.14%), Space: 342.4 MB (5.17%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        vector<int>freq(2e6+1,0);
+        int n=nums.size();
+        int maxi=1;
+        map<int,int>mp;
+        for(int i=0;i<n;i++){
+           int s=nums[i]-k;
+           int e=nums[i]+k;
+           mp[s]++;
+           mp[e+1]--;
+        }
+        vector<int>l_sweep;
+        int c=0;
+        for(auto it:mp){
+           if(c==0){
+            l_sweep.push_back(it.second);
+            c++;
+           }
+           else {
+            l_sweep.push_back(l_sweep.back()+it.second);
+           }
+        }
+        for(int i=0;i<l_sweep.size();i++){
+            maxi=max(maxi,l_sweep[i]);
+        }
+       
+        return maxi;
+
+        
+    }
+};
