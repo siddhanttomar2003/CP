c970c2effeffdf0518892ef05d35f65451225c5d
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2024-12-19 11:27:02 +0530

    Time: 0 ms (100%), Space: 9.1 MB (35.22%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -14,13 +14,22 @@ public:
         return true;
     }
     int maxChunksToSorted(vector<int>& arr) {
-        int tar=0;
-        int n=arr.size();
-        int count=0;
-        vector<int>vis(n,0);
+        // int tar=0;
+        // int n=arr.size();
+        // int count=0;
+        // vector<int>vis(n,0);
+        // for(int i=0;i<n;i++){
+        //     vis[arr[i]]=1;
+        //     if(check(vis))count++;
+        // }
+        // return count;
+        // second approach 
+        // at particular index i the max till that index is equal to that index that means all the elements from 0 to i are present already and we can choose it as a chunk
+        int count=0;int n=arr.size();
+        int maxi=-1;
         for(int i=0;i<n;i++){
-            vis[arr[i]]=1;
-            if(check(vis))count++;
+            maxi=max(maxi,arr[i]);
+            if(maxi==i)count++;
         }
         return count;
     }
