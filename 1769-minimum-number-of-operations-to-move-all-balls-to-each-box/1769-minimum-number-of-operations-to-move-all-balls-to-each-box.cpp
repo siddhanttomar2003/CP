15e2fe1feb764af499f6ced60f1f398612d5c407
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-01-06 12:52:34 +0530

    Time: 0 ms (100%), Space: 12.7 MB (15.16%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        int n=boxes.size();
-        vector<int>pre_cum(n,0),suff_cum(n,0);
-        int curr_s=0,curr_e=0;
-        for(int i=0;i<n;i++){
-            if(i>=1){
-                pre_cum[i]=(pre_cum[i-1]+curr_s);
-            }
-            else pre_cum[i]=curr_s;
-            if(n-i-1<n-1){
-                suff_cum[n-i-1]=(suff_cum[n-i]+curr_e);
-            }
-            else suff_cum[n-i-1]=curr_e;
-            if(boxes[i]=='1')curr_s++;
-            if(boxes[n-i-1]=='1')curr_e++;
+        int n = boxes.size();
+        vector<int> pre_cum(n, 0), suff_cum(n, 0);
+        int curr_s = 0, curr_e = 0;
+        for (int i = 0; i < n; i++) {
+            pre_cum[i] = curr_s + (i >= 1 ? pre_cum[i - 1] : 0);
+            suff_cum[n - i - 1] =
+                curr_e + (n - i - 1 < n - 1 ? suff_cum[n - i] : 0);
+            if (boxes[i] == '1')
+                curr_s++;
+            if (boxes[n - i - 1] == '1')
+                curr_e++;
         }
-        vector<int>ans(n,0);
-        for(int i=0;i<boxes.size();i++){
-              ans[i]=pre_cum[i]+suff_cum[i];
+        vector<int> ans(n, 0);
+        for (int i = 0; i < boxes.size(); i++) {
+            ans[i] = pre_cum[i] + suff_cum[i];
         }
         return ans;
     }
