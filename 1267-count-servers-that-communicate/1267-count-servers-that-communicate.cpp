32c78735d29b74fa10b3d288b31d92d61a809c5e
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-01-23 12:12:43 +0530

    Time: 11 ms (25.98%), Space: 30.5 MB (8.14%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,6 +1,7 @@
 class Dsu {
 public:
     vector<int> parent, size;
+
     Dsu(int n) {
         size.resize(n, 1);
         parent.resize(n);
@@ -8,17 +9,19 @@ public:
             parent[i] = i;
         }
     }
+
     int find_par(int node) {
         if (parent[node] == node)
             return node;
-        else
-            return parent[node] = find_par(parent[node]);
+        return parent[node] = find_par(parent[node]); // Path compression
     }
+
     void unions(int u, int v) {
         u = find_par(u);
         v = find_par(v);
         if (u == v)
             return;
+
         if (size[u] < size[v]) {
             parent[u] = v;
             size[v] += size[u];
@@ -28,26 +31,56 @@ public:
         }
     }
 };
+
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        Dsu d(m * n + 1);
+        Dsu dsu(n * m);
+
+        // Connect servers in rows and columns
         for (int i = 0; i < n; i++) {
+            int firstServer = -1;
             for (int j = 0; j < m; j++) {
                 if (grid[i][j]) {
-                    d.unions(i,n+j);   
+                    if (firstServer == -1)
+                        firstServer = i * m + j;
+                    else
+                        dsu.unions(firstServer, i * m + j);
                 }
             }
         }
-        int count = 0;
-        for(int i=0;i<m*n+1;i++){
-            if(d.size[i]>2){
-                count+=(d.size[i]-1);
+
+        for (int j = 0; j < m; j++) {
+            int firstServer = -1;
+            for (int i = 0; i < n; i++) {
+                if (grid[i][j]) {
+                    if (firstServer == -1)
+                        firstServer = i * m + j;
+                    else
+                        dsu.unions(firstServer, i * m + j);
+                }
             }
         }
+
+        // Count connected components with more than one server
+        unordered_map<int, int> componentCount;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j]) {
+                    int root = dsu.find_par(i * m + j);
+                    componentCount[root]++;
+                }
+            }
+        }
+
+        int count = 0;
+        for (auto& [root, size] : componentCount) {
+            if (size > 1)
+                count += size; // Only count servers in connected components
+        }
+
         return count;
     }
-
 };
