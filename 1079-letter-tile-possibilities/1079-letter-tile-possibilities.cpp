8f5728982130a6a28340d89f38898e071c7c97e7
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-02-17 11:41:09 +0530

    Time: 4 ms (62.18%), Space: 7.8 MB (97.61%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int buildstring(vector<int>&freq){
+        int ways = 0;
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                freq[i]--;
+                ways += 1 + buildstring(freq);
+                freq[i]++;
+            }
+        }
+        return ways;
+    }
+public:
+    int numTilePossibilities(string tiles) {
+        vector<int> freq(26,0);
+        for(char c:tiles){
+            freq[c-'A']++;
+        }
+        return buildstring(freq);
+    }
+};
