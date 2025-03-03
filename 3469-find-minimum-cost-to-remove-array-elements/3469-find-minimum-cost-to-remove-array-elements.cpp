a61f5ac2cad93f224c274c1bfdb9509800339d81
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-03-03 19:08:34 +0530

    Time: 127 ms (64.71%), Space: 31.8 MB (100%) - LeetHub

--- 3469-find-minimum-cost-to-remove-array-elements/3469-find-minimum-cost-to-remove-array-elements.cpp
+++ 3469-find-minimum-cost-to-remove-array-elements/3469-find-minimum-cost-to-remove-array-elements.cpp
@@ -1,31 +1,28 @@
+typedef long long ll;
 class Solution {
 public:
-    int cal(vector<int>&nums, int i, int last, map<int,map<int,int>>&dp){
+    ll dp[1001];
+    ll cal(vector<int>&nums, int i, int last){
         if(i>=nums.size())return nums[last];
-        if( dp.find(i)!=dp.end() && dp[i].find(last)!=dp[i].end())return dp[i][last];
-        int mini=1e9;
-        if(last==-1){
-         mini=min(mini,max(nums[i],nums[i+1])+cal(nums,i+3,i+2,dp));
-         mini=min(mini,max(nums[i],nums[i+2])+cal(nums,i+3,i+1,dp));
-         mini=min(mini,max(nums[i+1],nums[i+2])+cal(nums,i+3,i,dp));
-        }
-        else if(i+1>=nums.size()){
-            return dp[i][last]=max(nums[i],nums[last]);
+        if(dp[last]!=-1)return dp[last];
+        ll mini=1e10;
+         if(i+1>=nums.size()){
+            return dp[last]=max(nums[i],nums[last]);
         }
         else {
-            mini=min(mini,max(nums[last],nums[i])+cal(nums,i+2,i+1,dp));
-            mini=min(mini,max(nums[last],nums[i+1])+cal(nums,i+2,i,dp));
-            mini=min(mini,max(nums[i],nums[i+1])+cal(nums,i+2,last,dp));
+            mini=min(mini,max(nums[last],nums[i])+cal(nums,i+2,i+1));
+            mini=min(mini,max(nums[last],nums[i+1])+cal(nums,i+2,i));
+            mini=min(mini,max(nums[i],nums[i+1])+cal(nums,i+2,last));
         }
-        return dp[i][last]=mini;
+        return dp[last]=mini;
     }
     int minCost(vector<int>& nums) {
-        map<int,map<int,int>>dp;
         int n=nums.size();
         if(n==1)return nums[0];
         if(n==2){
             return max(nums[0],nums[1]);
         }
-        return cal(nums,0,-1,dp);
+        memset(dp,-1,sizeof(dp));
+        return min({max(nums[1],nums[2])+cal(nums,3,0),max(nums[0],nums[2])+cal(nums,3,1),max(nums[0],nums[1])+cal(nums,3,2)});
     }
 };
