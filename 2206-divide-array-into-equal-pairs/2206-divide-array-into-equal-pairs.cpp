f10ec8baed3600b3c9312a14d6514be106c3bd91
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-03-17 09:33:50 +0530

    Time: 10 ms (7.78%), Space: 18.2 MB (31.79%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+     int n=nums.size();
+     unordered_map<int,int>mp;
+     for(int i=0;i<n;i++)mp[nums[i]]++;
+     for(auto it:mp){
+        if(it.second&1)return false;
+     }
+     return true;
+    }
+};
