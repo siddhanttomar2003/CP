344e5b2b16230c5f4774fca4d9567f508ac3e1fc
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-04-19 08:57:01 +0530

    Time: 60 ms (39.66%), Space: 60.4 MB (73.14%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,23 +1,18 @@
+typedef long long ll;
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        long long ans=0;
-        int n=nums.size();
+        ll ans=0;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            int x=lower-nums[i];
-            long long start=lower_bound(nums.begin(),nums.end(),x)-nums.begin();
-            start=max((int)start,i+1);
-            int y=upper-nums[i];
-            long long end=upper_bound(nums.begin(),nums.end(),y)-nums.begin();
-            if(end==0){
-                continue;
-            }
-            end--;
-            if(end>=start){
-                ans+=(end-start+1);
-            }
+        for(int i=0;i<nums.size();i++){
+            ll n1=((ll)lower-(ll)(nums[i]));
+            ll n2=((ll)upper-(ll)(nums[i]));
+            ll ind1=lower_bound(nums.begin(),nums.end(),n1)-nums.begin();
+            ll ind2=upper_bound(nums.begin(),nums.end(),n2)-nums.begin();
+            if(ind2<=ind1)continue;
+            ans+=(ind2-ind1);
+            if(i>=ind1 && ind2-1>=i)ans--;
         }
-        return ans;
+        return ans/(2*1ll);
     }
 };
