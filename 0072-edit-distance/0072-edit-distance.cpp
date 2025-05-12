8fb6ac173529bc900ee596941fac9f6fa73bbfed
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-05-12 22:11:15 +0530

    Time: 11 ms (31.56%), Space: 13.2 MB (43.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    int dp[501][501];
-    int cal(string  &word1,string &word2, int i, int j){
-        if(i==word1.size() && j==word2.size())return 0;
-        if(i==word1.size() || j==word2.size())return max(word1.size()-i,word2.size()-j);
-        if(dp[i][j]!=-1)return dp[i][j];
-        int ways=1e8;
-        if(word1[i]==word2[j]){
-            ways=cal(word1,word2,i+1,j+1);
-        }
-        else  ways=min(ways,1+cal(word1,word2,i+1,j+1));
-        ways=min(ways,1+cal(word1,word2,i+1,j));
-        ways=min(ways,1+cal(word1,word2,i,j+1));
-       
-        
-        return dp[i][j]=ways;
-    }
     int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof(dp));
-        return cal(word1,word2,0,0);
+        int n=word1.size(),m=word2.size();
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1,1e9));
+        //dp[i][j]-> number of different ways to make the string a equal to string b
+        // if we take the string a to ith index and string b to jth index
+
+        // where will it fail during the transition :-
+        // when either one of this string completes or both of them completes we cannot call
+        // for further transition
+
+        //->base case:1(when string b finished )
+        // ->base case:2 (when string a finished)
+           
+        for(int i=0;i<=n;i++)dp[i][0]=i;
+        for(int i=0;i<=m;i++)dp[0][i]=i;
+        for(int i=1;i<=n;i++){
+            char c=word1[i-1];
+            for(int j=1;j<=m;j++){
+               char d=word2[j-1];
+               if(c==d){
+                dp[i][j]=dp[i-1][j-1];
+               }
+               else {
+                 dp[i][j]=min({1+dp[i-1][j-1],1+dp[i-1][j],1+dp[i][j-1]});
+               }
+            }
+        }
+        return dp[n][m];
     }
 };
