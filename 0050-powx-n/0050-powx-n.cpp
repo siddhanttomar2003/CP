67383c98223f056239f7d50135dfaf95f0cbd574
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-05-16 00:13:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-  
-    double myPow(double x, int n) {
-        double ans=1;
-        if(n<0){
-            x=1/x;
-        }
-        long num=labs(n);
-        
-        while(num>0){
-            if(num&1){
+    double res(double x, int n){
+        double ans=1.00;
+        while(n>0){
+            if(n&1){
                 ans*=x;
             }
-            x*=x;                                       
-            num>>=1;
+            x*=x;
+            n>>=1;
         }
         return ans;
-        
+    }
+    double myPow(double x, int n) {
+        if(n<0){
+            x=1.00/x;
+            n=abs(n);
+        }
+        return res(x,n);
     }
 };
