1e67e148268fd992135e6532bc67a194b1948994
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-05-22 12:18:25 +0530

    Time: 0 ms (100%), Space: 21.8 MB (46.63%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-       vector<vector<int>>ans;
-       bool check=false;
-       int i=0;
-       for(i;i<intervals.size();i++){
-          if(intervals[i][0]>newInterval[1]){
-            ans.push_back(newInterval);
-            check=true;
-            break;
-          }
-          else if(newInterval[0]>intervals[i][1]){
-            ans.push_back(intervals[i]);
-          }
-          else {
-                   int start=min(newInterval[0],intervals[i][0]);
-                   int end=max(newInterval[1],intervals[i][1]);
-                   check=true;
-                   i++;
-                   for(i;i<intervals.size();i++){
-                      int curr_s=intervals[i][0];
-                      int curr_e=intervals[i][1];
-                      if(curr_s<=end){
-                        end=max(end,curr_e);
-                      }
-                      else {
-                      
-                        break;
-                      }
-                   }
-                   ans.push_back({start,end});
-                   break;
-          }
-       }
-       for(i;i<intervals.size();i++)ans.push_back(intervals[i]);
-       if(!check)ans.push_back(newInterval);
-       return ans;
+        // inserting previous non overlapping
+        int i=0,n=intervals.size();
+        vector<vector<int>>ans;
+        while(i<n && newInterval[0]>intervals[i][1]){
+             ans.push_back(intervals[i]);
+             i++;
+        }
+        // merging overlapping one
+        if(i<n && intervals[i][0]<=newInterval[1]){
+              int start=min(intervals[i][0],newInterval[0]);
+              int end=max(intervals[i][1],newInterval[1]);
+              i++;
+              while(i<n && intervals[i][0]<=end){
+                  end=max(end,intervals[i][1]);
+                  i++;
+              }
+              ans.push_back({start,end});
+        }
+
+        else ans.push_back(newInterval);
+        // filling non overlapping at the ends
+        while(i<n)ans.push_back(intervals[i++]);
+        
+        return ans;
     }
 };
