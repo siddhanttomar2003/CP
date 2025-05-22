c6fb5ea2fd5bb8c5e60fd966f9cb7ae6d8e86839
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-05-22 12:01:00 +0530

    Time: 0 ms (100%), Space: 21.6 MB (94.04%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>>ans;
-        int s=intervals[0][0],e=intervals[0][1];
-        for(int i=1;i<intervals.size();i++){
-             int curr_s=intervals[i][0],curr_e=intervals[i][1];
-             if(curr_s<=e){
-                e=max(e,curr_e);
-             }
-             else {
-                ans.push_back({s,e});
-                s=curr_s,e=curr_e;
-             }
-        }
-        ans.push_back({s,e});
-        return ans;
+       vector<vector<int>>ans;
+       bool check=false;
+       int i=0;
+       for(i;i<intervals.size();i++){
+          if(intervals[i][0]>newInterval[1]){
+            ans.push_back(newInterval);
+            check=true;
+            break;
+          }
+          else if(newInterval[0]>intervals[i][1]){
+            ans.push_back(intervals[i]);
+          }
+          else {
+                   int start=min(newInterval[0],intervals[i][0]);
+                   int end=max(newInterval[1],intervals[i][1]);
+                   check=true;
+                   i++;
+                   for(i;i<intervals.size();i++){
+                      int curr_s=intervals[i][0];
+                      int curr_e=intervals[i][1];
+                      if(curr_s<=end){
+                        end=max(end,curr_e);
+                      }
+                      else {
+                      
+                        break;
+                      }
+                   }
+                   ans.push_back({start,end});
+                   break;
+          }
+       }
+       for(i;i<intervals.size();i++)ans.push_back(intervals[i]);
+       if(!check)ans.push_back(newInterval);
+       return ans;
     }
 };
