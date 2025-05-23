f27f92fe9240576b50287cf87fe19cb5c51ed2a7
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-05-23 15:37:03 +0530

    Time: 4 ms (11.11%), Space: 8.9 MB (42.92%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int>ans;
-        vector<int>start(26,-1),end(26,-1);
-        for(int i=0;i<s.size();i++){
-            int ind=s[i]-97;
-            if(start[ind]==-1)start[ind]=i;
-            end[ind]=i;
+        vector<int>f(26,-1),l(26,-1);
+        int n=s.size();
+        for(int i=0;i<n;i++){
+           if(f[s[i]-97]==-1)f[s[i]-97]=i;
+           l[s[i]-97]=i;
         }
-        int i=0;
-        while(i<s.size()){
-            int curr=1;
-            int curr_end=end[s[i]-97];
-            while(i<curr_end){
-                curr_end=max(curr_end,end[s[i]-97]);
-                i++;
-                curr++;
-            }
-            ans.push_back(curr);
-            i++;
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+           int curr=1;
+           int last=l[s[i]-97];
+           while(i<last && i<n){
+               last=max(last,l[s[i]-97]);
+               i++;
+               curr++;
+           }
+           ans.push_back(curr);
         }
         return ans;
     }
