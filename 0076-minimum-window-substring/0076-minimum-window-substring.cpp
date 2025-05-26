4a43b42edb8b3f76a979d13cab53eda002ed8796
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-05-27 02:10:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,52 +1,39 @@
+typedef long long ll;
 class Solution {
 public:
-    bool check(map<char,int>&mp1, map<char,int>&mp2){
-        for(auto it:mp1){
-            if(mp2.find(it.first)!=mp2.end()){
-                if(mp2[it.first]>=mp1[it.first])continue;
-                else return false;
-            }
-            else return false;
+    bool valid (vector<ll>&t,vector<ll>&f){
+        for(int i=0;i<256;i++){
+            if(t[i]<f[i])return false;
         }
         return true;
     }
     string minWindow(string s, string t) {
-        if(t.size()>s.size())return "";
-        map<char,int>mp1;
-        map<char,int>mp2;
-        int j; int i=0;
-        for( j=0;j<t.size();j++){
-         mp1[t[j]]++;
-         mp2[s[j]]++;
-        }
         string ans="";
-        int len=INT_MAX;
-        int pos_start= -1;
-        int pos_end= -1;
-        int n=s.size();
-        j--;
-        if(check(mp1,mp2)){
-            len=t.size();
-            pos_start=0;
-            pos_end=len-1;
+        ll start=-1,end=-1;
+        ll len=1e10;
+        if(s.size()<t.size())return ans;
+        vector<ll>freq(256,0),temp(256,0);
+        for(auto it:t)freq[it]++;
+        ll j=0;
+        ll i=0;
+        ll n=s.size();
+        while(j<n){
+           temp[s[j]]++;
+           while(valid(temp,freq) && i<=j){
+              ll curr_len=j-i+1;
+              if(curr_len<len){
+                len=curr_len;
+                start=i;
+                end=j;
+              }
+              temp[s[i]]--;
+              i++;
+           }
+           j++;
         }
-        while(j+1<n){
-          j++;
-          mp2[s[j]]++;
-          while(check(mp1,mp2) && j>=i){
-            if(j-i+1<len){
-                pos_start=i;
-                pos_end=j;
-                len=j-i+1;
-            }
-            mp2[s[i]]--;
-            if(mp2[s[i]]==0)mp2.erase(s[i]);
-            i++;
-          }
+        for(int i=start;i<=end;i++){
+            ans+=s[i];
         }
-        
-        if(pos_start==-1)return "";
-        for(int i=pos_start;i<=pos_end;i++){ans+=s[i];}
         return ans;
     }
 };
