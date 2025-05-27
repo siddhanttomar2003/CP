7218b1a9a130bf65c870bc010270fb6b29f8ff58
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-05-27 19:45:23 +0530

    Time: 1 ms (86.68%), Space: 12.9 MB (89.03%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    void cal(vector<string>&ans, int i, int n, int o, string a){
+    void cal(vector<string>&ans, int i, int n, int o, string &a){
       if(i==n){
         if(o==0)ans.push_back(a);
         return;
       }
       if(o>0){
-        cal(ans,i+1,n,o-1,a+')');
+        a+=')';
+        cal(ans,i+1,n,o-1,a);
+        a.pop_back();
       }
-      cal(ans,i+1,n,o+1,a+'(');
+      a+='(';
+      cal(ans,i+1,n,o+1,a);
+      a.pop_back();
     }
     vector<string> generateParenthesis(int n) {
         int open=0;
         vector<string>ans;
-        cal(ans,0,2*n,open,"");
+        string a="";
+        cal(ans,0,2*n,open,a);
         return ans;
     }
 };
