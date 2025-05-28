25aa8bec4ddeee9562f483bce9b6f5e5987b420b
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-05-28 15:23:42 +0530

    Time: 2572 ms (5.07%), Space: 578.8 MB (5.07%) - LeetHub

--- 3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3372-maximize-the-number-of-target-nodes-after-connecting-trees-i/3372-maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,60 +1,47 @@
 class Solution {
 public:
-    int dfs(int start, vector<int>& visited, vector<vector<int>>& adj, int k) {
-        if (k < 0)
-            return 0;
-        int curr = 1;
-        visited[start] = 1;
-        for (int i = 0; i < adj[start].size(); i++) {
-            if (!visited[adj[start][i]]) {
-                curr += dfs(adj[start][i], visited, adj, k - 1);
+    int bfs(int start, vector<vector<int>>&adj,int n,int k){
+         if(k<0)return 0;
+         vector<int>vis(n,0);
+         queue<vector<int>>q;
+         q.push({0,start});
+         int count=0;
+         vis[start]=1;
+         while(!q.empty()){
+            int par=q.front()[1];
+            int curr_dis=q.front()[0];
+            q.pop();
+            count++;
+            for(auto it:adj[par]){
+                if(!vis[it] && curr_dis+1<=k){
+                      vis[it]=1;
+                      q.push({curr_dis+1,it});
+                }
             }
-        }
-        return curr;
-    }
-    int cal(int start, vector<vector<int>>& adj, int n, int k) {
-        vector<int> visited(n, 0);
-        return dfs(start, visited, adj, k);
-    }
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1,
-                               vector<vector<int>>& edges2, int k) {
-        int maxi1 = 0;
-        for (int i = 0; i < edges1.size(); i++) {
-            maxi1 = max({maxi1, edges1[i][0], edges1[i][1]});
-        }
-        int maxi2 = 0;
-        for (int i = 0; i < edges2.size(); i++) {
-            maxi2 = max({maxi2, edges2[i][0], edges2[i][1]});
-        }
-        vector<vector<int>> adj1(maxi1 + 1);
-        vector<vector<int>> adj2(maxi2 + 1);
+         }
+         return count;
 
-        for (int i = 0; i < edges1.size(); i++) {
-            int a = edges1[i][0];
-            int b = edges1[i][1];
-            adj1[a].push_back(b);
-            adj1[b].push_back(a);
-        }
-        for (int i = 0; i < edges2.size(); i++) {
-            int a = edges2[i][0];
-            int b = edges2[i][1];
-            adj2[a].push_back(b);
-            adj2[b].push_back(a);
-        }
-        vector<int> ans(maxi1 + 1);
-        vector<int> first(maxi1 + 1);
-        for (int i = 0; i < maxi1 + 1; i++) {
-            first[i] = cal(i, adj1, maxi1 + 1, k);
-            // cout << first[i] << " ";
-        }
-        int mm2=0;
-        for(int i=0;i<maxi2+1;i++){
-            mm2=max(mm2,cal(i,adj2,maxi2+1,k-1));
-        }
-        for(int i=0;i<maxi1+1;i++){
-            ans[i]=mm2+first[i];
-        }
-        return ans;
-       
+    }
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
+         int n=edges1.size();
+         int m=edges2.size();
+         vector<vector<int>>adj1(n+1),adj2(m+1);
+         for(auto it:edges1){
+            adj1[it[0]].push_back(it[1]);
+            adj1[it[1]].push_back(it[0]);
+         }
+         for(auto it:edges2){
+            adj2[it[0]].push_back(it[1]);
+            adj2[it[1]].push_back(it[0]);
+         }
+         int maxi=0;
+         for(int i=0;i<=m;i++){
+            maxi=max(maxi,bfs(i,adj2,m+1,k-1));
+         }
+         vector<int>ans(n+1);
+         for(int i=0;i<=n;i++){
+            ans[i]=bfs(i,adj1,n+1,k)+maxi;
+         }
+         return ans;
     }
 };
