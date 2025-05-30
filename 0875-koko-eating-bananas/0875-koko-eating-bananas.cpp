1fcfdd0084e1a54aa5290dad864e98e6fb03c623
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-05-30 09:59:20 +0530

    Time: 11 ms (43.67%), Space: 22.8 MB (96.52%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    int cal(vector<int> &piles,int &mid, int &h){
-        long long ans=0;
-        for(int i=0;i<piles.size();i++){
-            if(piles[i]%mid==0)ans+=piles[i]/mid;
-            else ans+=piles[i]/mid+1;
-           if(ans>h)return -1;
+    typedef long long ll;
+    bool check(ll m, vector<int>&p, int h){
+        ll count=0;
+        for(auto it:p){
+            count+=((it*1ll)/m+(it%m==0?0:1));
         }
-        if(ans==h)return 0;
-        else return 1;
+        return count<=h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int i=1;
-        int e=*max_element(piles.begin(),piles.end());
-        int ans=e;
-        while(i<=e){
-            int mid=i+(e-i)/2;
-            int ansi=cal(piles,mid,h);
-             if(ansi==0){
-                ans=min(ans,mid);
-                e=mid-1;
-             }
-             else if(ansi==-1){
-                i=mid+1;
-             }
-             else {
-                ans=min(ans,mid);
-                e=mid-1;
-             }
+        ll low=1;
+        ll high=*max_element(piles.begin(),piles.end());
+        ll ans=high;
+        while(low<=high){
+           ll m=low+(high-low)/2;
+           if(check(m,piles,h)){
+             ans=m;
+             high=m-1;
+           }
+           else low=m+1;
         }
-      return ans;
-        }
+        return ans;
+    }
 };
