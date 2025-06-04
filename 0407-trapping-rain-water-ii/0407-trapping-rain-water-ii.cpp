900b8d33d718bb655f8d2d555fc51b351a0a2563
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-04 10:34:43 +0530

    Time: 67 ms (23.07%), Space: 24.3 MB (22.54%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    bool check(int cx,int cy, int n, int m){
-        if(cx>=0 && cy>=0 && cx<n && cy<m)return true;
-        return false;
-    }
-    int trapRainWater(vector<vector<int>>& h) {
-        int ans=0;
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        int n=heightMap.size();
+        int m=heightMap[0].size();
+        vector<vector<int>>contri(n,vector<int>(m,0));
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-        int n=h.size();int m=h[0].size();
-        map<pair<int,int>,int>mp;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 || i==n-1 || j==0 || j==m-1){
-                    pq.push({h[i][j],i,j});
-                    mp[{i,j}]++;
-                }
-            }
+            pq.push({heightMap[i][0],i,0});
+            pq.push({heightMap[i][m-1],i,m-1});
+            contri[i][0]=1;
+            contri[i][m-1]=1;
         }
+        for(int i=1;i<m-1;i++){
+          pq.push({heightMap[0][i],0,i});
+          pq.push({heightMap[n-1][i],n-1,i});
+          contri[0][i]=1;
+          contri[n-1][i]=1;
+        }
+        int ans=0;
+        int level=0;
         while(pq.size()>0){
-            int curr=pq.top()[0];
-            int curr_x=pq.top()[1];
-            int curr_y=pq.top()[2];
+             level=max(level,pq.top()[0]);
+            int curr_r=pq.top()[1];
+            int curr_c=pq.top()[2];
+            int delRow[]={-1,0,1,0};
+            int delCol[]={0,1,0,-1};
             pq.pop();
-            // mp[{curr_x,curr_y}]++;
-            int delRow[]={1,0,-1,0};
-            int delCol[]={0,-1,0,1};
             for(int i=0;i<4;i++){
-                int nx=curr_x+delRow[i];
-                int ny=curr_y+delCol[i];
-                if(check(nx,ny,n,m) && mp.find({nx,ny})==mp.end()){
-                    ans+=max(curr-h[nx][ny],0);
-                    mp[{nx,ny}]++;
-                    pq.push({max(curr,h[nx][ny]),nx,ny});
+                int n_r=curr_r+delRow[i];
+                int n_c=curr_c+delCol[i];
+                if(n_r<n && n_r>=0 && n_c<m && n_c>=0 && !contri[n_r][n_c] ){
+                    contri[n_r][n_c]=1;
+                    ans+=(max(0,level-heightMap[n_r][n_c]));
+                    pq.push({max(level,heightMap[n_r][n_c]),n_r,n_c});
                 }
             }
         }
-        
         return ans;
     }
 };
