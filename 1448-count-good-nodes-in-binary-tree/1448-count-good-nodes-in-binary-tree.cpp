6609f36a4b9dbd604fff83ac977cd39b2d551fa7
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-06 00:33:21 +0530

    Time: 90 ms (85.33%), Space: 88.3 MB (61.72%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,29 +11,16 @@
  */
 class Solution {
 public:
-     void recur(TreeNode *root,int &ans,int &prev,int maxi){
-         if(root==NULL)return;
-         if(root->left==NULL && root->right==NULL){
-             if(root->val>=prev){
-            ans++;
-             }
-             return;
-         }
-         if(root->val>=prev){
-             ans++;prev=root->val;
-             maxi=max(maxi,root->val);
-         }
-         recur(root->left,ans,prev,maxi);
-          prev=maxi;
-         recur(root->right,ans,prev,maxi);
-        
-
-     }
+    void cal(TreeNode *root, int &count, int maxi){
+        if(root==NULL)return;
+        if(root->val>=maxi)count++;
+        maxi=max(maxi,root->val);
+        cal(root->left,count,maxi);
+        cal(root->right,count,maxi);
+    }
     int goodNodes(TreeNode* root) {
-        int ans=0;
-        int prev=INT_MIN;
-        int maxi=INT_MIN;
-        recur(root,ans,prev,maxi);
-        return ans;
+        int count=0;
+        cal(root,count,-1e5);
+        return count;
     }
 };
