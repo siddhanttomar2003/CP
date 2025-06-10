b8987eac780b780ad2cd7435edf60ab1a66aea64
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-10 11:41:10 +0530

    Time: 2 ms (44.05%), Space: 9.3 MB (82.95%) - LeetHub

--- 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int maxDifference(string s) {
-        map<char,int>mp;
-        for(int i=0;i<s.size();i++){
-            mp[s[i]]++;
+        int maxi_odd = 0, mini_even = 1e6;
+        vector<int> freq(26, 0);
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i] - 97]++;
         }
-        vector<int>odd,even;
-        for(auto it:mp){
-            if(it.second&1)odd.push_back(it.second);
-            else even.push_back(it.second);
+        for (int i = 0; i < 26; i++) {
+            if(freq[i]!=0){
+            if (freq[i] & 1)
+                maxi_odd = max(maxi_odd, freq[i]);
+            else
+                mini_even = min(mini_even, freq[i]);
+            }
         }
-        sort(even.begin(),even.end());sort(odd.begin(),odd.end());
-        int s1=even.size();int s2=odd.size();
-
-        return odd[s2-1]-even[0];
+        return maxi_odd-mini_even;
     }
 };
