77e0b628ce3c34a224520f69c7fe8c776531ce12
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-11 16:22:24 +0530

    Time: 960 ms (33.21%), Space: 14.8 MB (99.37%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool isValid(int i, int j, int n, int m, vector<vector<int>>&vis){
-        return i>=0 && i<n && j>=0 && j<m && !vis[i][j];
+    bool isValid(int i, int j, int n, int m, vector<vector<char>>&b){
+        return i>=0 && i<n && j>=0 && j<m && b[i][j]!='#';
     }
-    void cal(int i, int j, vector<vector<char>>&b, string &curr, unordered_set<string>&ans, int len,vector<vector<int>>&vis,unordered_map<string,int>&mp){
+    void cal(int i, int j, vector<vector<char>>&b, string &curr, unordered_set<string>&ans, int len,unordered_map<string,int>&mp){
          if(len>=10)return;
-         vis[i][j]=1;
+
          int delRow[]={-1,0,1,0};
          int delCol[]={0,1,0,-1};
          int n=b.size(),m=b[0].size();
          curr+=b[i][j];
+         b[i][j]='#';
          if(mp.find(curr)!=mp.end())ans.insert(curr);
          for(int k=0;k<4;k++){
             int n_r=i+delRow[k];
             int n_c=j+delCol[k];
-            if(isValid(n_r,n_c,n,m,vis)){
-                cal(n_r,n_c,b,curr,ans,len+1,vis,mp);
+            if(isValid(n_r,n_c,n,m,b)){
+                cal(n_r,n_c,b,curr,ans,len+1,mp);
             }
          }
-         vis[i][j]=0;
+         b[i][j]=curr.back();
          curr.pop_back();
     }
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
@@ -29,8 +30,7 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 string curr="";
-                vector<vector<int>>vis(n,vector<int>(m,0));
-                cal(i,j,board,curr,ans,0,vis,mp);
+                cal(i,j,board,curr,ans,0,mp);
             }
         }
         vector<string>f_ans;
