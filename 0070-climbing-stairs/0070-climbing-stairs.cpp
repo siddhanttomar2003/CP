6fdd2f3d29887d2e104202e78ca9d51341468c59
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-17 02:25:25 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-     int find_ans(vector<int> &dp,int n){
-        if(n==0)return 1;
+    int dp[46];
+    int cal(int n){
         if(n<0)return 0;
+        if(n==0)return 1;
         if(dp[n]!=-1)return dp[n];
-        return dp[n]=find_ans(dp,n-1)+find_ans(dp,n-2);
-     }
+        return dp[n]=cal(n-1)+cal(n-2);
+    }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-       return find_ans(dp,n);
+        memset(dp,-1,sizeof(dp));
+        return cal(n);
     }
 };
