fdc6a4d70716fe548bd97594d585aeabe41648bb
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-18 16:59:52 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,17 @@
+typedef long long ll ;
 class Solution {
 public:
+    ll cal(ll n, ll r){
+        ll res=1;
+        for(int i=0;i<r;i++){
+            res*=(n-i);
+            res/=(i+1);
+        }
+        return res;
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[0][0]=1;
-        for(int i=0;i<n;i++){
-            dp[0][i]=1;
-        }
-        for(int i=0;i<m;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<m;i++){
-           for(int j=1;j<n;j++){
-             dp[i][j]=dp[i-1][j]+dp[i][j-1];
-           }
-        }
-        return dp[m-1][n-1];
+       ll t=m+n-2;
+       ll mini=min(n-1,m-1);
+       return cal(t,mini);
     }
 };
