c9cbf905a89a3c332648c2d695f574ea27705c98
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-20 12:21:36 +0530

    Time: 3 ms (87.84%), Space: 12.8 MB (65.05%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int dp[21][2001];
-    int cal(vector<int>&nums, int tar, int i, int curr){
-     if(i==nums.size()){
-        return curr==tar;
-     }
-     if(dp[i][curr+1000]!=-1)return dp[i][curr+1000];
-     int ways=0;
-     if(curr+nums[i]<=1000){
-     ways+=cal(nums,tar,i+1,curr+nums[i]);
-     }
-     if(curr-nums[i]>=-1000){
-     ways+=cal(nums,tar,i+1,curr-nums[i]);
-     }
-     return dp[i][curr+1000]=ways;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        memset(dp,-1,sizeof(dp));
-        return cal(nums,target,0,0);
+        // s1+s2 = sum;
+        // s1-s2 = target
+        // 2s1 = sum + target
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int f_tar= sum + target;
+        if(f_tar & 1) return 0;
+        int n = nums.size();
+        f_tar /= 2;
+        vector<int>dp(2001,0);
+        dp[1000] = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = f_tar; j >= 0 ;j--){
+                if(nums[i] <= j){
+                    dp[j+1000] += dp[j-nums[i]+1000];
+                }
+            }
+        }
+        return dp[f_tar+1000];
     }
 };
