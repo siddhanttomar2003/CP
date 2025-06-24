3292e5539eb88579927addc6828cd1920ca1a6b8
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-24 12:41:11 +0530

    Time: 0 ms (100%), Space: 26.6 MB (73.29%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,33 +1,24 @@
+typedef long long ll;
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int>ans;
-        int n=grid.size();
-        int size=n*n;
-        for(int i=0;i<n;i++){
-          for(int j=0;j<n;j++){
-             ans.push_back(grid[i][j]);
-          }
-        }
-        int missing=0;
-        vector<int>f;
-        int dup=0;
-        for(int i=0;i<size;i++){
-            if(ans[i]!=i+1){
-                 if(ans[i]==ans[ans[i]-1]){
-                    dup=ans[i];
-                 }
-                 else {
-                    swap(ans[i],ans[ans[i]-1]);
-                    i--;
-                 }  
+        ll size = grid.size();
+        ll n = size * size;
+        ll sum = (n * (n + 1))/2;
+        ll ac_sum = 0;
+        ll sum2 = (n * (n + 1) * (2 * n + 1))/6;
+        ll ac_sum2 = 0;
+        for(auto it : grid){
+            for(auto it2 : it){
+                ac_sum += it2;
+                ac_sum2 += (it2 * it2);
             }
         }
-        for(int i=0;i<size;i++){
-            if(ans[i]!=i+1)missing=i+1;
-        }
-        f.push_back(dup);
-        f.push_back(missing);
-        return f;
+        ll rhs1 = sum - ac_sum;
+        ll rhs2 = sum2 - ac_sum2;
+        ll rhs3 = rhs2 / rhs1;
+        int missing = (rhs3 + rhs1)/2;
+        int twice = rhs3 - missing;
+        return {twice , missing};
     }
 };
