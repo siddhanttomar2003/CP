89c942fe016346a9aa7edc553843fbb46373b570
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-29 18:25:53 +0530

    Time: 0 ms (100%), Space: 9.8 MB (13.91%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-      string st;
-      int n=s.length();
-     int count=0;
-     for(int i=0;i<n;i++){
-         if(s[i]=='('){
-             if(count>=1)st+=s[i];
-             count++;
-         }
-         else {
-        if(count>1)st+=s[i];
-        count--;
-         }
-         
-     }
-     return st;
-
+        int n = s.size();
+        vector<int> vis(n, 0);
+        stack<int> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                st.push(i);
+            }
+            else {
+                int ind = st.top();
+                st.pop();
+                if(st.size() == 0){
+                    vis[i] = 1;
+                    vis[ind] = 1;
+                }
+            }
+        }
+        string ans = "";
+        for(int i = 0; i < s.size(); i++){
+            if(!vis[i])ans += s[i];
+        }
+        return ans;
     }
 };
