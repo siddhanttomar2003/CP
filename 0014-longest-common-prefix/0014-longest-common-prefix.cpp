2787fad3fc7b690dc064d966ea03ba434587b73c
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-29 18:58:11 +0530

    Time: 0 ms (100%), Space: 12.8 MB (8.29%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,94 +1,33 @@
 class TrieNode {
-     public:
-     TrieNode *child[26];
-     bool isExist;
-     TrieNode (){
-        for(int i=0;i<26;i++){
-            child[i]=NULL;
-        }
-        isExist=false;
-     }
-};
-class Trie {
 public:
-      TrieNode *root;
-    Trie() {
-        root=new TrieNode();
+    vector<TrieNode*> child;
+    int count;
+    TrieNode() {
+        child.resize(26, NULL);
+        count = 0;
     }
-
-    
-    void insert(string word) {
-        TrieNode *curr=root;
-        for(int i=0;i<word.size();i++){
-            int index=word[i]-'a';
-            if(curr->child[index]==NULL){
-                curr->child[index]=new TrieNode();
-            }
-            curr=curr->child[index];
-        }
-        curr->isExist=true;
-    }
-    
-    bool search(string word) {
-        TrieNode *curr=root;
-        for(int i=0;i<word.size();i++){
-            int index=word[i]-'a';
-            if(curr->child[index]==NULL)return false;
-            curr=curr->child[index];
-        }
-        return curr->isExist;
-    }
-    
-    bool startsWith(string word) {
-          TrieNode *curr=root;
-        for(int i=0;i<word.size();i++){
-            int index=word[i]-'a';
-            if(curr->child[index]==NULL)return false;
-            curr=curr->child[index];
-        }
-
-        return true;
-    }
-   string checksize(){
-    TrieNode *curr=root;
-    bool flag=true;
-    // int ans=0;
-    string temp="";
-    TrieNode *tempo=root;
-    while(flag){
-        int tempc=0;
-        
-        for(int i=0;i<26;i++){
-              if(curr->child[i]!=NULL){
-                tempc++;
-                temp+=(char)(97+i);
-                tempo=curr->child[i];
-              }
-              if(tempc>=2){
-                temp.pop_back();temp.pop_back();
-                flag=false;break;
-              }
-        }
-
-        curr=tempo;
-        if(curr->isExist==true){
-            flag=false;break;
-        }
-        // ans++;
-    }
-    return temp;
-   }
 };
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        // if(strs.size()==0)return "";
-        Trie t;
-        for(int i=0;i<strs.size();i++){
-            if(strs[i].size()==0)return "";
-            t.insert(strs[i]);
+        int n = strs.size();
+        int maxi = 0;
+        TrieNode * root = new TrieNode();
+        string ans = "";
+        for(auto it : strs){
+            TrieNode * curr = root;
+            string temp = "";
+            for(auto ch : it){
+                temp += ch;
+                int c = ch - 97;
+                if(curr -> child[c] == NULL){
+                    curr -> child[c] = new TrieNode();
+                }
+                curr = curr -> child[c];
+                curr -> count ++ ;
+                if(curr -> count == n)ans = temp;
+            }
         }
-       string fans= t.checksize();
-      return fans;
+        return ans;
     }
 };
