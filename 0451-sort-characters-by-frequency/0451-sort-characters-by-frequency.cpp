62663e79c3df73470258da40d0c1b81e78ece059
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-06-29 19:52:13 +0530

    Time: 87 ms (5.05%), Space: 11.3 MB (43.01%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     string frequencySort(string s) {
         map<char,int>mp;
-        for(auto it:s)mp[it]++;
-        sort(s.begin(),s.end(),[&mp](char a, char b){
-             if(mp[a]>mp[b])return true;
-             return false;
-        }); 
+        for(auto it : s)mp[it]++;
+        sort(s.begin(), s.end(), [&mp](char a, char b){
+               if(mp[a] != mp[b])return mp[a] > mp[b];
+               return a > b;
+        });
         return s;
     }
-
 };
