e1381be8e7ab9a9a6e5dae24e61414edc27d6796
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-07-01 23:27:31 +0530

    Time: 167 ms (5.05%), Space: 116.7 MB (97.16%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,24 +1,39 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        string s="";
-        ListNode *temp=head;
-        while(temp!=NULL){
-         s+=temp->val;
-         temp=temp->next;
+        // Steps to follow:
+        // 1_) Find the middle element
+        ListNode *slow = head, *fast = head;
+        while(fast!=NULL && fast->next !=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        string t=s;
-        reverse(s.begin(),s.end());
-        return t==s;
+        // 2_) if the no of nodes are odd then move slow to one point
+        if(fast != NULL && fast->next == NULL){
+            slow = slow->next;
+        }
+        //3_) Reverse the end half
+        ListNode *prev = NULL;
+        ListNode *temp = NULL;
+        while(slow != NULL && slow->next != NULL){
+            temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        if(slow != NULL){
+            slow->next = prev;
+        }
+        //4_) Compare the start half and end half if found any inequality then return false otherwise return true.
+        fast = head;
+        while(slow && fast){
+            if(slow->val != fast->val){
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return true;
+
     }
 };
