97a2aebffd9217022aba5199b33337d2099270b4
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-07-04 01:02:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
-        int i=0;
-        int j=0;
-        int m=g.size();
-        int n=s.size();
-        int count=0;
-        while(i<m&&j<n){
-            if(s[j]>=g[i]){
+        map<int,int>mp;
+        int count = 0;
+        for(auto it : s)mp[it]++;
+        for(auto it : g){
+            int req = it;
+            auto it2 = mp.lower_bound(it);
+            if(it2 != mp.end()){
+                mp[it2->second]--;
+                if(mp[it2->second] == 0)mp.erase(it2->first);
                 count++;
-                j++;
-                i++;
             }
-            else {
-                j++;
-            }
-
         }
         return count;
     }
