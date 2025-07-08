6850bacb16e80c1c9120bc1c59d711d8043a1258
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-07-08 19:51:14 +0530

    Time: 169 ms (43.21%), Space: 126 MB (24.51%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    vector<int>ex;
+    int cal(vector<vector<int>>& e, int i, int k, int n, vector<int> &ex) {
+        if (i == n || k == 0) return 0;
+        if (dp[i][k] != -1) return dp[i][k];
+        int ans = cal(e, i + 1, k, n, ex);  
+        if (k > 0) {
+            int num = e[i][1];
+            int ind = upper_bound(ex.begin(), ex.end(), num) - ex.begin();
+            ans = max(ans, e[i][2] + cal(e, ind, k - 1, n, ex)); 
+        }
+        return dp[i][k] = ans;
+    }
+
     int maxValue(vector<vector<int>>& events, int k) {
         int n = events.size();
-        sort(events.begin(), events.end());  // Sort by start time
-        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
-        vector<int> start_times(n);
-        for (int i = 0; i < n; i++) {
-            start_times[i] = events[i][0];
+        ex.assign(n, 0);
+        sort(events.begin(), events.end());
+        for(int i = 0; i < n; i++){
+            ex[i] = events[i][0];
         }
-
-        for (int i = n - 1; i >= 0; i--) {
-            int next = upper_bound(start_times.begin(), start_times.end(), events[i][1]) - start_times.begin();
-            for (int j = 1; j <= k; j++) {
-                dp[i][j] = max(
-                    dp[i + 1][j],                      
-                    events[i][2] + dp[next][j - 1]     
-                );
-            }
-        }
-
-        return dp[0][k];
+        dp.assign(n, vector<int>(k + 1 , -1));
+        return cal(events, 0, k, n, ex);
     }
 };
