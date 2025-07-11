42f8de30ab76555ac7201b6737950861416bb97e
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-07-11 16:01:27 +0530

    Time: 226 ms (7.76%), Space: 139.8 MB (5.46%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -1,42 +1,47 @@
 class Solution {
 public:
+    typedef long long ll;
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        int len=size(meetings);
-        vector<long long>endtime(n,0);
-        vector<int>count(n,0);
-        sort(begin(meetings),end(meetings));
-        for(int i=0;i<len;i++){
-            int start=meetings[i][0];
-            int time=meetings[i][1]-start;
-            bool isdelay = true;
-            long long mindelay=LLONG_MAX;
-            int indx;
-            for(int j=0;j<n;j++){
-                if(endtime[j]<=start){
-                    endtime[j]=meetings[i][1];
-                    count[j]++;
-                    isdelay = false;
-                    break;
-                }else{
-                    if(endtime[j]<mindelay){
-                        mindelay=endtime[j];
-                        indx=j;
-                    }
-                }
+        map<ll,ll>mp;
+        set<ll>st;
+        for(ll i = 0; i < n; i++)st.insert(i);
+        priority_queue<vector<ll>, vector<vector<ll>>, greater<vector<ll>>>pq;
+        sort(meetings.begin(), meetings.end());
+        ll start = meetings[0][0];
+        ll end = meetings[0][1];
+        pq.push({end, 0, start});
+        mp[0]++;
+        st.erase(0);
+        cout << 0 << endl;
+        ll size = meetings.size();
+        for(ll i = 1; i < size; i++){
+            ll curr_start = meetings[i][0];
+            ll curr_dur = meetings[i][1] - meetings[i][0];
+            while(!pq.empty() && pq.top()[0] <= curr_start){
+                ll idx = pq.top()[1];
+                st.insert(idx);
+                pq.pop();
             }
-            if(isdelay){
-                endtime[indx]+=time;
-                count[indx]++;
+            if(st.size() == 0){
+                ll idx  = pq.top()[1];
+                st.insert(idx);
+                curr_start = max(curr_start, pq.top()[0]);
+                pq.pop();
             }
+            auto it = st.begin();
+            ll idx = *it;
+            // cout << idx << endl;
+            st.erase(idx);
+            mp[idx] ++;
+            pq.push({curr_dur + curr_start, idx, curr_start});
         }
-        
-        int ans=0;
-        for(int i=0;i<n;i++){
-            cout<<count[i]<<" ";
-            if(count[i]>count[ans]){
-                ans =i;
-            }
+        ll maxi = -1;
+        for(auto it : mp){
+            maxi = max(maxi, it.second);
         }
-        return ans;
+        for(auto it : mp){
+            if(it.second == maxi)return it.first;
+        }
+        return -1;
     }
 };
