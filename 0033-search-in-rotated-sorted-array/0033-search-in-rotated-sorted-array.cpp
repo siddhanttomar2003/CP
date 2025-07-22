32693ca48c69895e3a65b4197f312cf7868f9f9b
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-07-22 19:21:55 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.31%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int i = 0;
-        int j = n - 1;
+    int search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int i = 0, j = n - 1;
         while(i <= j){
-            int mid = (i + (j - i) / 2);
-            // cout<<i<<" "<<j<<endl;
-            if(nums[mid] == target)return mid;
-            if(nums[mid] >= nums[i]){
-                if(target >= nums[i] && target < nums[mid])j = mid - 1;
-                else i = mid + 1;
+            int mid = i + (j - i) / 2;
+            // check left half sorted
+            if(target == arr[mid])return mid;
+            if(arr[mid] >= arr[0]){
+                if(target >= arr[0] && target <= arr[mid]){
+                        j = mid - 1;
+                }
+                else {
+                    i = mid + 1;
+                }
             }
             else {
-                if(target > nums[mid] && target <= nums[j])i = mid + 1;
+                if(target >= arr[mid] && target <= arr[j]){
+                    i = mid + 1;
+                }
                 else j = mid - 1;
             }
         }
