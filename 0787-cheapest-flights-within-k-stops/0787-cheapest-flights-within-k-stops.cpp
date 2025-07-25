7984242ef2ad45dd791f6856637de9b3ec79325a
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-07-25 19:40:58 +0530

    Time: 42 ms (6.44%), Space: 23.3 MB (5.2%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
+
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>>adj(n);
-        for(auto it:flights){
-            adj[it[0]].push_back({it[1],it[2]});
+        for(auto it : flights){
+            adj[it[0]].push_back({it[1], it[2]});
         }
-        vector<vector<int>>dis(n,vector<int>(k+1,1e9));
-        dis[src][0]=0;
+        vector<vector<int>>dis(n, vector<int>(k + 1, 2e9));
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-        pq.push({0,0,src});
-        while(pq.size()>0){
-            auto it=pq.top();
+        pq.push({0, 0, src});
+        dis[src][0] = 0;
+        while(pq.size() > 0){
+            auto it = pq.top();
+            int c = it[0];
+            int stops = it[1];
+            int node = it[2];
             pq.pop();
-            int curr_cost=it[0];
-            int stops=it[1];
-            int par=it[2];
-            for(auto item:adj[par]){
-                int child=item.first;
-                int wt=item.second;
-                if(dis[child][stops]>wt+curr_cost && stops<=k){
-                    dis[child][stops]=wt+curr_cost;
-                    if(stops+1<=k){
-                    pq.push({dis[child][stops],stops+1,child});
-                    }
+            for(auto it2 : adj[node]){
+                int child = it2.first;
+                int curr = it2.second;
+                if(dis[child][stops] > c + curr){
+                    dis[child][stops] = c + curr;
+                    if(stops + 1 <= k)
+                    pq.push({c + curr, stops + 1, child});
                 }
             }
         }
-        int mini=1e9;
-        for(int i=0;i<=k;i++){
-            mini=min(mini,dis[dst][i]);
+        int ans = 2e9;
+        for(auto it : dis[dst]){
+            ans = min(ans, it);
         }
-        if(mini==1e9)return -1;
-        return mini;
+        if(ans == 2e9)return -1;
+        return ans;
     }
 };
