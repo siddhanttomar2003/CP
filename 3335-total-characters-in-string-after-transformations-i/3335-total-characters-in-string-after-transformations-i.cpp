ed2e924b35c9e2fc349d9738d76bdba2aae7ff7d
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-07-25 23:46:08 +0530

    Time: 863 ms (26.95%), Space: 495.1 MB (7.96%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     const int m = 1e9 + 7;
-
     int lengthAfterTransformations(string s, int t) {
-        vector<int> freq(26, 0);
-        for (int i = 0; i < s.size(); i++) {
-            freq[s[i] - 'a']++;
+        vector<int>freq(26, 0);
+        for(int i = 0; i < s.size(); i++){
+            freq[s[i] - 97]++;
         }
-
-        for (int i = 0; i < t; i++) {
-            vector<int> n_f(26, 0);
-            for (int j = 0; j < 26; j++) {
-                
-                    
-                    if (j != 25) {
-                        n_f[j + 1] = (n_f[j + 1] + freq[j]) % m;
-                    } else {
-                        n_f[0] = (n_f[0] + freq[j]) % m;
-                        n_f[1] = (n_f[1] + freq[j]) % m;
-                    }
-                
+        // for(int i = 0; i < 26; i++)cout << freq[i] << " ";
+        // cout << endl;
+        for(int i = 0; i < t; i++){
+            vector<int>temp(26, 0);
+            for(int j = 1; j < 25 ; j++){
+                temp[j] = freq[j - 1];
             }
-            freq = n_f;
+            temp[25] = freq[24];
+            temp[0] = freq[25];
+            temp[1] = (freq[25] + temp[1]) % m;
+            // for(int j = 0; j < 26; j++)cout << temp[j] <<" ";
+            // cout << endl;
+            freq = temp;
         }
-
         int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            ans = (ans + freq[i]) % m;
+        for(int i = 0; i < 26; i++){
+                ans = (ans + freq[i]) % m;
         }
-
         return ans;
     }
+
 };
