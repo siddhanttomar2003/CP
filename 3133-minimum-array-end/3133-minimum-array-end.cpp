765bc81b51691907d3fb019519a6c58b88baa9ca
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-07-29 00:45:02 +0530

    Time: 0 ms (100%), Space: 8.7 MB (84.6%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
+    typedef long long ll;
     long long minEnd(int n, int x) {
-        long long num=x;
-        long long times=n-1;
-        set<int>st;
-        long long ans=0;
-        vector<int>v1(64,0);
-        vector<int>v2(64,0);
-        for(long long i=0;i<64;i++){
-             if(num&(1LL<<i))v1[64-i-1]=1;
-             if(times&(1LL<<i))v2[64-i-1]=1;
+        ll fixed = x;
+        ll ans = x;
+        ll num = n - 1;
+        ll j = 0;
+        for(ll i = 0; i < 64; i++){
+            if(!(fixed & (1LL << i))){
+                if(!(num & (1LL << j))){
+                    j++;
+                }
+                else {
+                    ans |= (1LL << i);
+                    j++;
+                }
+            }
         }
-         int j=0;
-        for(long long i=0;i<64;i++){
-           if(!(v1[64-i-1])){
-               if(v2[64-j-1]){
-                 ans|=(1<<i);
-               }
-               j++;
-           }
-           else {
-             ans|=(1<<i);
-           }
-        }
-         return ans;
-
+        return ans;
     }
 };
