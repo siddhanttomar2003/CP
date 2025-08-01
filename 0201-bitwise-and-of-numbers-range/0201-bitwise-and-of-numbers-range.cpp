e80a1ee9437c0ccb15e9c8ef4d38ce5707d6bc7a
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-08-02 02:31:52 +0530

    Time: 7 ms (7.77%), Space: 11.3 MB (47.14%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int binaryToDecimal(string n) {
-        string num = n;
-        int dec_value = 0;
-        long long base = 1;
-        int len = num.length();
-        for (int i = len - 1; i >= 0; i--) {
-            if (num[i] == '1')
-                dec_value += base;
-            base = base * 2;
-        }
-        return dec_value;
-    }
+    typedef long long ll;
     int rangeBitwiseAnd(int left, int right) {
-        string ans = "";
-        int i;
-        for (i = 1; i <= 32; i++) {
-           if((right&(1<<(32-i)))!=(left&(1<<(32-i)))){
-                break;
-           }
-           else {
-               if(right&(1<<(32-i)))ans+='1';
-               else ans+='0';
-           }
+        ll num = 1;
+        while(num <= right){
+            if(num > left && num <= right)return 0;
+            num *= 2ll;
         }
-        while (i <= 32) {
-            ans += '0';
-            i++;
+        int pos = -1;
+        for(int i = 0; i < 32; i++){
+            if((right & (1 << i))  && !(left & (1 << i)))pos = i;
         }
-        int f_ans=binaryToDecimal(ans);
-        return f_ans;
+        // cout << pos << endl;
+        int ans = 0;
+        for(int i = 0; i < 32; i++)
+        {
+            if((right & (1 << i)) && (left & (1 << i)) && pos < i){
+                    ans |= (1 << i);
+            }
+        }
+        return ans;
     }
 };
