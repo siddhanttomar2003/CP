4c96c1cd69446330eb679d9cb3885c4c163f8cdd
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-08-10 09:49:34 +0530

    Time: 391 ms (21.85%), Space: 23.8 MB (19.06%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-   
-    int ladderLength(string start, string end, vector<string>& wordList) {
-        map<string,vector<string>>adj;
-        set<string>words(wordList.begin(),wordList.end());
-        if(words.find(end)==words.end())return 0;
-        wordList.push_back(start);
-        map<string,int>dis;
-        for(auto it:wordList){
-            string temp=it;
-            dis[temp]=1e9;
-             for(int i=0;i<temp.size();i++){
-                for(int j=0;j<26;j++){
-                    char c= temp[i];
-                    temp[i]=(char)(97+j);
-                    if(words.find(temp)!=words.end()){
-                        adj[it].push_back(temp);
-                    }
-                    temp[i]=c;
-                }
-             }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        map<string,int>mp;
+        for(auto it : wordList){
+            mp[it]++;
         }
-        priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>>pq;
-        dis[start]=0;
-        pq.push({0,start});
-        while(pq.size()>0){
-            int curr_dis=pq.top().first;
-            string par=pq.top().second;
-            pq.pop();
-            for(auto it:adj[par]){
-                if(dis[it]>curr_dis+1){
-                    dis[it]=curr_dis+1;
-                    if(it==end)return curr_dis+2;
-                    pq.push({curr_dis+1,it});
-                    
-                }
+        mp[beginWord]++;
+        if(mp.find(endWord) == mp.end())return 0;
+        queue<pair<int,string>>q;
+        map<string,int>dis;
+        q.push({0, beginWord});
+        dis[beginWord] = 0;
+        while(q.size() > 0){
+            int curr = q.front().first;
+            string temp = q.front().second;
+            q.pop();
+            for(int i = 0; i < temp.size(); i++){
+                 for(int j = 0; j < 26; j ++){
+                    char c = temp[i];
+                    temp[i] = (char)(j + 97);
+                    if(mp.find(temp) != mp.end() && dis.find(temp) == dis.end()){
+                        if(temp == endWord)return curr + 2;
+                        dis[temp]++;
+                        q.push({curr + 1, temp});
+                    }
+                    temp[i] = c;
+                 }
             }
         }
         return 0;
