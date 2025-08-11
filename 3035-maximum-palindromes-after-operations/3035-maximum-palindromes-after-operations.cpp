322ef28779c2c39f8e7d43cdace4a3c8499648f4
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-08-11 10:28:53 +0530

    Time: 4 ms (72.36%), Space: 32.8 MB (18.59%) - LeetHub

--- 3035-maximum-palindromes-after-operations/3035-maximum-palindromes-after-operations.cpp
+++ 3035-maximum-palindromes-after-operations/3035-maximum-palindromes-after-operations.cpp
@@ -1,60 +1,36 @@
 class Solution {
 public:
     int maxPalindromesAfterOperations(vector<string>& words) {
-        vector<int>v(26,0);
-        for(int i=0;i<words.size();i++){
-            for(auto it:words[i])v[it-97]++;
+        vector<int>sz;
+        for(auto it : words){
+            sz.push_back((int)it.size());
         }
-        sort(words.begin(),words.end(),[](string &a,string &b){
-             return a.size()<b.size();
-        });
-        int ans=0;
-        for (int i = 0; i < words.size(); i++) {
-            int req = words[i].size();
-            bool odd = req & 1;
-            if (odd)
-                req--;
-            vector<int> tempo = v;
-            for (int j = 0; j < 26; j++) {
-                if (v[j] % 2 == 0 && v[j] > 0) {
-                    int mini = min(v[j], req);
-                    v[j] -= mini;
-                    req -= mini;
-                } else if (v[j] & 1) {
-                    int mini = min(v[j] - 1, req);
-                    v[j] -= mini;
-                    req -= mini;
-                }
-                if (req == 0 && odd == 0) {
-                    ans++;
+        sort(sz.begin(), sz.end());
+        vector<int>freq(26, 0);
+        for(auto it : words){
+            for(auto it2 : it)freq[it2 - 97]++;
+        }
+        int count = 0;
+        for(auto it : sz){
+            int req = it;
+            if(req & 1)req--;
+            for(auto & it2 : freq){
+                 int avail = it2;
+                 if(it2 & 1)avail--;
+                 if(avail <= req){
+                    it2 -= avail;
+                    req -= avail;
+                 }
+                 else {
+                    it2 -= req;
+                    req = 0;
+                 }
+                 if(req == 0){
+                    count++;
                     break;
-                }
-            }
-            if (odd && req == 0) {
-                for (int j = 0; j < 26; j++) {
-                    if (v[j] & 1) {
-                        odd = 0;
-                        v[j]--;
-                        ans++;
-                        break;
-                    }
-                }
-                if (odd) {
-                    for (int j = 0; j < 26; j++) {
-                        if (v[j] > 0) {
-                            v[j]--;
-
-                            ans++;
-                            break;
-                        }
-                    }
-                }
-            }
-            if (req != 0 || odd) {
-                v = tempo;
+                 }
             }
         }
-        return ans;
-
+        return count;
     }
 };
