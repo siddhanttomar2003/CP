4892458ad1b7e1cabe70f060090d269be6c7fb29
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-08-12 09:40:52 +0530

    Time: 676 ms (77.12%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
-with temp as(
-    select p.product_id as product_id,p.product_name as product_name,o.unit as unit,o.order_date as order_date from Products p
-    LEFT JOIN 
-    Orders o on p.product_id = o.product_id
-)
--- select * from temp;
-select product_name ,sum(unit) as unit from temp 
-where YEAR(order_date) = 2020 and MONTH(order_date) = 2
-group by product_id having sum(unit) >= 100;
+select p1.product_name, sum(o1.unit) as unit
+from 
+Products p1
+LEFT JOIN
+Orders o1
+on p1.product_id = o1.product_id where YEAR(o1.order_date) = '2020' and MONTH(o1.order_date) = '2' group by p1.product_id having sum(o1.unit) >= 100;
