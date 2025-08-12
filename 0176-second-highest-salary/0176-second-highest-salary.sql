695c44e9dc083a619dff04806661e793ef043653
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-08-12 08:40:51 +0530

    Time: 261 ms (72.92%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,2 @@
 # Write your MySQL query statement below
-with temp as (
-    select salary , dense_rank() over (order by salary desc)  as ranks from Employee
-)
--- select * from temp;
-SELECT COALESCE((select  salary  from temp where ranks = 2 limit 1 ),NULL) as SecondHighestSalary;
+select coalesce((select distinct salary from Employee order by salary desc limit 1 offset 1), NULL) as SecondHighestSalary;
