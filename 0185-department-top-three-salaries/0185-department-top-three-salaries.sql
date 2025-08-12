cf080ee079a158afe3af0ca5a16627ad8c8607ac
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-08-12 10:08:24 +0530

    Time: 1085 ms (37.73%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,11 +1,18 @@
-# Write your MySQL query statement below
-with temp as (
-    select *,dense_rank() OVER (partition by departmentId order by salary desc) as ranks from Employee 
+WITH temp AS (
+    SELECT 
+        e1.departmentId,
+        e1.name AS emp,
+        e1.salary,
+        d1.name AS department,
+        Dense_RANK() OVER (PARTITION BY d1.name ORDER BY e1.salary DESC) AS ra
+    FROM Employee e1
+    LEFT JOIN Department d1
+        ON e1.departmentId = d1.id
+
 )
-select d.name as Department , t.name as Employee, t.salary as Salary  from temp as t
-LEFT JOIN Department as d
-on t.departmentId = d.id
-where  t.ranks  in (1,2,3)
-order by t.salary desc;
-
-
+SELECT 
+    t.department AS Department,
+    t.emp AS Employee,
+    t.salary AS Salary
+FROM temp t
+WHERE t.ra <= 3  order by t.salary desc;
