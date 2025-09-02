862ca4490c453cacaf426ed6eb12f4a0cc50ac81
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-09-02 17:45:20 +0530

    Time: 63 ms (14.81%), Space: 32.4 MB (97.78%) - LeetHub

--- 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    bool check(int j1, int j2,vector<vector<int>>& points){
-        int n = points.size();
-        int x1 = points[j1][0];
-        int y1 = points[j1][1];
-        int x2 = points[j2][0];
-        int y2 = points[j2][1];
-        
-        for(int i=0; i<n; i++){
-            if(i == j1 || i == j2) continue;
-            int x = points[i][0];
-            int y = points[i][1];
-            
-            if(x>=x1 && x <= x2 && y <= y1 && y >= y2) return false;
-        }
-        return true;
-    }
-    
     int numberOfPairs(vector<vector<int>>& points) {
-        int n = points.size(), ans = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i == j) continue;
-                int x1 = points[i][0];
-                int y1 = points[i][1];
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-                if(x2 < x1 || y2 > y1) continue;
-                
-                if( (((y2-y1) >= 0 && (x2-x1) <= 0)  || ((y2-y1) <= 0 && (x2-x1) >= 0)) && check(i,j,points) ) ans++;
+        sort(points.begin(), points.end(), [](vector<int>&a, vector<int>&b){
+            if(a[0] != b[0])return a[0] < b[0];
+            return a[1] > b[1];
+        });
+        for(int i = 0; i < points.size(); i++){
+            cout << points[i][0] << " " << points[i][1] << endl;
+        }
+        int ans = 0;
+        for(int i = 0; i < points.size(); i++){
+            int maxi = points[i][1];
+            int mini = -1e9;
+            for(int j = i + 1; j < points.size(); j++){
+                if(points[j][1] <= maxi){
+                    if(points[j][1] > mini){
+                        mini = max(mini, points[j][1]);
+                        ans++;
+                    }
+                }
             }
         }
         return ans;
