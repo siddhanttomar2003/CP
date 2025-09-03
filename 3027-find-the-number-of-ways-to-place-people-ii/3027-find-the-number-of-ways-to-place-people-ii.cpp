fdb02f9cb86619177808d152961b64e73c0a08f2
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-09-03 12:08:24 +0530

    Time: 145 ms (42.55%), Space: 41.1 MB (67.02%) - LeetHub

--- 3027-find-the-number-of-ways-to-place-people-ii/3027-find-the-number-of-ways-to-place-people-ii.cpp
+++ 3027-find-the-number-of-ways-to-place-people-ii/3027-find-the-number-of-ways-to-place-people-ii.cpp
@@ -1,30 +1,24 @@
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds; 
-#define ordered_set tree<long long, null_type,less<long long>, rb_tree_tag,tree_order_statistics_node_update> 
-typedef long long ll;
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-         sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-        if (a[0] != b[0])
-            return a[0] < b[0];
-        return a[1] > b[1];      
-    });
-        int n = points.size();
-        int count = 0;
-        
-        for (int i = 0; i < n; i++) {
-            ll y1 = points[i][1];
-            ll maxi=-1e18;
-            for (int j = i+1; j < n; j++) {
-                ll y2 = points[j][1];
-                if (y2 <= y1 ) {
-                   if(maxi<y2)count++;
-                   maxi=max(maxi,y2);
+        sort(points.begin(), points.end(), [](vector<int>&a, vector<int>&b){
+            if(a[0] != b[0])return a[0] < b[0];
+            return a[1] > b[1];
+        });
+      
+        int ans = 0;
+        for(int i = 0; i < points.size(); i++){
+            int maxi = points[i][1];
+            int mini = -2e9;
+            for(int j = i + 1; j < points.size(); j++){
+                if(points[j][1] <= maxi){
+                    if(points[j][1] > mini){
+                        mini = max(mini, points[j][1]);
+                        ans++;
+                    }
                 }
             }
         }
-        return count;
+        return ans;
     }
 };
