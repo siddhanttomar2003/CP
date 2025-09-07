4b2f338ab6dc53c5bdde85c76552a5a71774f758
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-09-07 18:55:29 +0530

    Time: 0 ms (100%), Space: 8.2 MB (77.47%) - LeetHub

--- 2749-minimum-operations-to-make-the-integer-zero/2749-minimum-operations-to-make-the-integer-zero.cpp
+++ 2749-minimum-operations-to-make-the-integer-zero/2749-minimum-operations-to-make-the-integer-zero.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    typedef long long ll;
     int makeTheIntegerZero(int num1, int num2) {
-        if (num2==0) return popcount(unsigned(num1));
-        long long x=num1;
-        for(int k=1; k<36; k++){
-            x-=num2;
-            if (x<k) return -1;
-            if (x>=0 && k>=popcount((unsigned long long)(x))) return k;
+        if(num1 <= num2)return -1;
+        for(ll i = 1; i <= 60; i++){
+            ll lhs = num1 - (i * num2);
+            if(lhs <= 0)break;
+            int count = 0;
+            for(ll j = 0; j <= 60; j++){
+                if(lhs & (1LL << j)){
+                    count++;
+                }
+            }
+            if(count <= i  && i <= lhs)return i;
         }
         return -1;
     }
