729df74d7fb49d13b56e8f767e44d7e89e4e47f4
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-10-11 13:16:57 +0530

    Time: 132 ms (80.53%), Space: 151.8 MB (38.61%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int find_max(vector<int> &energy,int k,vector<int> &dp,int i,int n){
-        if(i>=n)return 0;
-        if(i==n-1)return energy[n-1];
-       if(dp[i]!=-1e8)return dp[i];
-        int take=energy[i]+find_max(energy,k,dp,i+k,n);
-        return dp[i]=take;
-    }
     int maximumEnergy(vector<int>& energy, int k) {
-        int n=energy.size();
-        int ans=INT_MIN;
-        vector<int>dp(n,-1e8);
-        for(int i=0;i<n;i++){
-            ans=max(ans,find_max(energy,k,dp,i,n));
-            cout<<ans<<endl;
-         }
-        
-        return ans;
+        int n = energy.size();
+        vector<int>pre(n, 0);
+        int maxi = -1e9;
+        for(int i = n - 1; i >= n - k; i--){
+            pre[i] = energy[i];
+            maxi = max(maxi, pre[i]);
+        }
+        for(int i = n - k - 1; i >= 0; i--){
+            pre[i] += (pre[i + k] + energy[i]);
+            maxi = max(maxi, pre[i]);
+        }
+        return maxi;
     }
 };
