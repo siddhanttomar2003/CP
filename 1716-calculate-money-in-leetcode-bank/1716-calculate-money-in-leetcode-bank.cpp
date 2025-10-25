1ac3a6e116b64c76a32e8a24d291165f700a1251
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-10-25 12:01:32 +0530

    Time: 0 ms (100%), Space: 8 MB (13.49%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int count=0;
-        int ans=0;
-        int a=1;
-        while(a<=n){
-         if(a%7==1){
-             count++;
-             ans+=count;
-         }
-         else if(a%7==0){
-             ans+=count+7-1;
-         }
-         else {
-             ans+=count+a%7-1;
-         }
-         a++;
+        int curr = 0;
+        int ans = 0;
+        for(int i = 1; i <= n; i++){
+            if(i % 7 == 1)curr++;
+            ans += (curr + (i % 7 == 0 ? 7 : i % 7) - 1);
+            // cout << curr + (i % 7) - 1 << endl;
         }
         return ans;
-
-         
-        }
+    }
 };
