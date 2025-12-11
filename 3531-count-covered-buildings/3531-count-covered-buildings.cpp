689f1d6e4d10f4d5d3d721c2218cafa3f15603a6
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2025-12-11 11:39:41 +0530

    Time: 2168 ms (5.52%), Space: 484.3 MB (5.52%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.cpp
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        map<int,set<int>>mp_x,mp_y;
-        for(auto it:buildings){
-            mp_x[it[0]].insert(it[1]);
-            mp_y[it[1]].insert(it[0]);
+    int countCoveredBuildings(int n, vector<vector<int>>& b) {
+        map<int, set<int>>x, y;
+        for(auto it : b){
+            x[it[0]].insert(it[1]);
+            y[it[1]].insert(it[0]);
         }
-        int count=0;
-        for(auto it:buildings){
-            int x=it[0];
-            int y=it[1];
-            auto it1=mp_x[x].upper_bound(y);
-            bool check=true;
-            if(it1==mp_x[x].end())check=false;
-            auto it2=mp_x[x].begin();
-            if(*it2==y)check=false;
-            auto it3= mp_y[y].upper_bound(x);
-            if(it3==mp_y[y].end())check=false;
-            auto it4= mp_y[y].begin();
-            if(*it4==x)check=false;
-            if(check)count++;
+        int ans = 0;
+        for(auto it : b){
+            int curr_x = it[0];
+            int curr_y = it[1];
+            auto it1 = x[curr_x].upper_bound(curr_y);
+            auto it2 = x[curr_x].lower_bound(curr_y);
+            auto it3 = y[curr_y].lower_bound(curr_x);
+            auto it4 = y[curr_y].upper_bound(curr_x);
+            if(it1 == x[curr_x].end() || it2 == x[curr_x].begin() || it3 == y[curr_y].begin() || it4 == y[curr_y].end())continue;
+            ans++;
         }
-        return count;
+        return ans;
     }
 };
