7d998a8ad1c884174048d25d6518b2682b579d67
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2026-01-18 22:29:54 +0530

    Time: 179 ms (24.13%), Space: 12.2 MB (100%) - LeetHub

--- 1895-largest-magic-square/1895-largest-magic-square.cpp
+++ 1895-largest-magic-square/1895-largest-magic-square.cpp
@@ -1,73 +1,74 @@
 class Solution {
 public:
-    // Check if k-size square can fit within grid bounds
     bool check(int i, int j, int k, int n, int m) {
-        return (i + k <= n && j - k + 1 >= 0);
+        if (i + k > n ||  j - k +1< 0)
+            return false;
+        return true;
     }
-
-    // Check if the k x k grid starting at (i, j) is a magic square
-    bool exist(int i, int j, int k, vector<vector<int>>& grid) {
+    bool exist(int i, int j, int k, int n, int m, vector<vector<int>>& grid) {
+        // check row;
+        // first row set as a template to check everything
         int sum = 0;
-        
-        // First row sum (template)
         for (int l = j; l > j - k; l--) {
             sum += grid[i][l];
         }
-
-        // Check rows for the same sum
         for (int l = i; l < i + k; l++) {
             int curr = 0;
             for (int g = j; g > j - k; g--) {
                 curr += grid[l][g];
             }
-            if (curr != sum) return false;
+            if(curr!=sum)return false;
         }
 
-        // Check columns for the same sum
-        for (int l = j; l > j - k; l--) {
-            int curr = 0;
-            for (int g = i; g < i + k; g++) {
-                curr += grid[g][l];
+        // check for every col
+        for(int l=j-k+1;l<j;l++){
+            int curr=0;
+            for(int g=i;g<i+k;g++){
+                curr+=grid[g][l];
             }
-            if (curr != sum) return false;
+            if(curr!=sum)return false;
         }
 
-        // Check left diagonal (top-right to bottom-left)
+        // check for left diagonal
         int curr_l = 0;
-        for (int l = 0; l < k; l++) {
-            curr_l += grid[i + l][j - l];
+        int l = i;
+        int r = j;
+        int t = k;
+        while (t--) {
+            curr_l += grid[l][r];
+            l++;
+            r--;
         }
-        if (curr_l != sum) return false;
-
-        // Check right diagonal (top-left to bottom-right)
+        if (curr_l != sum)
+            return false;
+        // check for right diagonal
+        j = j - k+ 1;
         int curr_r = 0;
-        for (int l = 0; l < k; l++) {
-            curr_r += grid[i + l][j - k + 1 + l];
+        int g = k;
+        while (g--) {
+            curr_r += grid[i][j];
+            i++;
+            j++;
         }
-        if (curr_r != sum) return false;
-
+        if (curr_r != sum)
+            return false;
         return true;
     }
-
     int largestMagicSquare(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int ans = 1;
-
-        // Iterate through all possible top-left corners (i, j)
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                // Try squares of increasing size k
-                for (int k = 2; k <= min(n, m); k++) {
+                for (int k = 2; k <= min(n,m); k++) {
                     if (check(i, j, k, n, m)) {
-                        if (exist(i, j, k, grid)) {
+                        if (exist(i, j, k, n, m, grid)) {
                             ans = max(ans, k);
                         }
                     }
                 }
             }
         }
-
         return ans;
     }
 };
