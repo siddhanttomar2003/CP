69fd0d62a30684deea9edc363a98c13776e379d5
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2026-02-07 16:31:47 +0530

    Time: 115 ms (5.47%), Space: 34.7 MB (33.73%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    int minimumDeletions(string s) {
-        stack<char>st;
-        int ans=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='b')st.push(s[i]);
-            else {
-                if(st.size()==0)continue;
-                else {
-                    st.pop();
-                    ans++;
-                }
-            }
+    int dp[100001][3];
+    int cal(string &s, int i, int n, int state){
+        if(i == n)return 0;
+        if(dp[i][state] != -1)return dp[i][state];
+        int ans = 1e9;
+        if(state == 0){
+            ans = min(ans, 1 + cal(s, i + 1, n, state));
+            if(s[i] == 'a')ans = min(ans, cal(s, i + 1, n, 1));
+            else if(s[i] == 'b')ans = min(ans, cal(s, i + 1, n, 2));
         }
-        return ans;
+        else if(state == 1){
+            if(s[i] == 'a')ans = min(ans, cal(s, i + 1, n, 1));
+            else if(s[i] == 'b')ans = min(ans, cal(s, i + 1, n, 2));
+            ans = min(ans, 1 + cal(s, i + 1, n, state));
+        }
+        else {
+            if(s[i] == 'b')ans = min(ans, cal(s, i + 1, n, 2));
+            ans = min(ans, 1 + cal(s, i + 1, n, state));
+        }
+        return dp[i][state] = ans;
+    }
+    int minimumDeletions(string s) {
+        int n = s.size();
+        memset(dp, -1, sizeof(dp));
+        return cal(s, 0, n, 0);
     }
 };
