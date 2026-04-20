f0c99ed1d33e185f4a48805ce67b93f9d86638df
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2026-04-20 22:08:57 +0530

    Time: 0 ms (100%), Space: 12.1 MB (40.24%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -5,8 +5,9 @@ public:
         if(c[0] != c[n - 1])return n - 1;
         int maxi = 0;
         for(int i = n - 1; i > 0; i--){
-            if(c[i] != c[0])maxi = i;
+            if(c[i] != c[0]){maxi = i;
             break;
+            }
         }
         for(int i = 0; i < n - 1; i++){
             if(c[i] != c[n - 1]){
