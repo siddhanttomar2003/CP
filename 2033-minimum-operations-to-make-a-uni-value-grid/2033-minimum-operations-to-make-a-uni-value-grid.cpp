abf88b25662ada397bfa1982eb652b8205ef25dc
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2026-04-29 00:31:17 +0530

    Time: 48 ms (32.51%), Space: 91.2 MB (7.82%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int>temp;
-        int n=grid.size(),m=grid[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                temp.push_back(grid[i][j]);
+        int n = grid.size(), m = grid[0].size();
+        set<int>st;
+        vector<int>ans;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                st.insert(grid[i][j] % x);
+                if(st.size() > 1)return -1;
+                ans.push_back(grid[i][j]);
             }
         }
-        sort(temp.begin(),temp.end());
-        int size=temp.size();
-        int median=-1,median2=-1;
-        int ans=0;
-        int ans2=0;
-        if(size&1){
-            median=temp[size/2];
+        sort(ans.begin(), ans.end());
+        vector<int>pre(n * m + 1, 0);
+        for(int i = 0; i < ans.size(); i++){
+            pre[i + 1] = pre[i] + ans[i];
         }
-        else {
-            median=temp[size/2-1];
-            median2=temp[size/2];
+        int mini = 1e9;
+        for(int i = 0; i < ans.size(); i++){
+            int left = pre[i], right = pre[n * m] - pre[i + 1];
+            int curr = ans[i];
+            mini = min(mini, (((i * curr) - (left)) / x) +  (right - ((n * m - (i + 1)) * curr)) / x);
         }
-    
-        for(int i=0;i<size;i++){
-            int req=abs(median-temp[i]);
-            if(req%x!=0)return -1;
-            ans+=req/x;
-        }
-        bool flag2=true;
-        if(size%2==0){
-            for(int i=0;i<size;i++){
-            int req=abs(median2-temp[i]);
-            if(req%x!=0)flag2=false;
-            ans2+=req/x;
-            }
-        }
-
-        if(size&1)return ans;
-        if(!flag2)return -1;
-        return min(ans,ans2);
-
+        return mini;
     }
 };
