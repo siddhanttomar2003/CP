1f66b23f64abc321ff5c09d7d84c712a44da327c
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2026-05-05 22:15:03 +0530

    Time: 0 ms (100%), Space: 16.4 MB (63.05%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,61 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* rev_ll(ListNode * head){
-        if(head == NULL ||  head -> next == NULL)return head;
-        ListNode * mhead = rev_ll(head -> next);
-        head -> next -> next = head;
-        head -> next = NULL;
-        return mhead;
-    }
-    int get_len(ListNode * head){
-        ListNode * temp = head;
+    int len(ListNode* head){
         int count = 0;
+        ListNode *temp = head;
         while(temp != NULL){
-            count++;
             temp = temp -> next;
+            count++;
         }
         return count;
     }
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head ==  NULL )return nullptr;
-        int len = get_len(head);
-        k %= len;
+        int n = len(head);
+        if(n == 0)return nullptr;
+        if(n == 1)return head;
+        k = k % n;
         if(k == 0)return head;
-        head = rev_ll(head);
-        ListNode * temp = head;
-        ListNode * last_head = head;
-        ListNode * nhead = head;
-        // while(temp != NULL){
-        //     cout << temp -> val << " ";
-        //     cout << endl;
-        //     temp = temp -> next;
-        // }
-        while(k--){
-            if(k == 0){
-               ListNode * store = temp -> next;
-               ListNode * curr = last_head;
-               ListNode * prev = NULL;
-               while(curr != store){
-                 ListNode * next = curr -> next;
-                 curr -> next = prev;
-                 prev = curr;
-                 curr = next;
-               }
-               head = prev;
-               ListNode * curr2 = store;
-               ListNode * prev2 = NULL;
-               while(curr2 != NULL){
-                 ListNode * next = curr2 -> next;
-                 curr2 -> next = prev2;
-                 prev2 = curr2;
-                 curr2 = next;
-               }
-               last_head -> next = prev2;
-               return head;
-            }
+        ListNode *temp = head;
+        int save = n - k;
+        ListNode* prev = NULL;
+        while(temp != NULL && save--){
+            prev = temp;
             temp = temp -> next;
         }
+        while(temp != NULL && temp -> next != NULL){
+            temp = temp -> next;
+        }
+        temp -> next = head;
+        head = prev -> next;
+        prev -> next = NULL;
         return head;
     }
 };
