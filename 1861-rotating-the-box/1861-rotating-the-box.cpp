ae728041f86c4b77be4fb0fc680f00f2040dd00d
Siddhant Tomar <141900451+siddhanttomar2003@users.noreply.github.com>
2026-05-07 00:22:06 +0530

    Time: 180 ms (5.16%), Space: 64.2 MB (5.41%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    static vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        const int r=box.size(), c=box[0].size();
-        vector<vector<char>> rotate(c, vector<char>(r, '.'));
-        for(int i=0; i<r; i++){
-            int bottom=c-1;
-            for(int j=c-1; j>=0; j--){
-                if (box[i][j]=='#'){
-                    rotate[bottom][r-1-i]='#';
-                    bottom--;
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& b) {
+        int n = b.size(), m = b[0].size();
+        vector<vector<char>>ans(m, vector<char>(n, '.'));
+        for(int i = 0; i < n; i++){
+            queue<int>q;
+            for(int j = m - 1; j >= 0; j--){
+                if(b[i][j] == '.')q.push(j);
+                else if(b[i][j] == '*'){
+                    while(q.size())q.pop();
+                    ans[j][n - i - 1] = '*';
                 }
-                else if (box[i][j]=='*'){
-                    rotate[j][r-1-i]='*';
-                    bottom=j-1;
+                else {
+                    b[i][j] = '.';
+                    q.push(j);
+                    ans[q.front()][n - i - 1] = '#';
+                    q.pop();
                 }
             }
         }
-        return rotate;
+        return ans;
     }
 };
-
-
-
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
